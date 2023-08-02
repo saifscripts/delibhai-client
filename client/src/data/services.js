200ea298aa283663a7f19035d61fd648a581e164
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2023-08-03 03:27:59 +0600

    rename services icon name in import

--- client/src/data/services.js
+++ client/src/data/services.js
@@ -1,18 +1,18 @@
-import dHero from '../assets/icons/bike.svg';
-import bloodLagbe from '../assets/icons/bloodlagbe.svg';
+import dhero from '../assets/icons/bike.svg';
+import bloodlagbe from '../assets/icons/bloodlagbe.svg';
 import delifood from '../assets/icons/delifood.svg';
-import dLabour from '../assets/icons/dlabour.svg';
-import dSend from '../assets/icons/dsend.svg';
-import dShop from '../assets/icons/dshop.svg';
-import gasLagbe from '../assets/icons/gaslagbe.svg';
+import dlabour from '../assets/icons/dlabour.svg';
+import dsend from '../assets/icons/dsend.svg';
+import dshop from '../assets/icons/dshop.svg';
+import gaslagbe from '../assets/icons/gaslagbe.svg';
 
 const services = [
-  { id: 1, icon: dHero, title: 'dHero', to: 'dhero' },
-  { id: 2, icon: dLabour, title: 'dLabour', to: 'dlabour' },
-  { id: 3, icon: dSend, title: 'dSend', to: 'dsend' },
-  { id: 4, icon: dShop, title: 'dShop', to: 'dshop' },
-  { id: 5, icon: bloodLagbe, title: 'Blood Lagbe?', to: 'bloodlagbe' },
-  { id: 6, icon: gasLagbe, title: 'Gas Lagbe?', to: 'gaslagbe' },
+  { id: 1, icon: dhero, title: 'dHero', to: 'dhero' },
+  { id: 2, icon: dlabour, title: 'dLabour', to: 'dlabour' },
+  { id: 3, icon: dsend, title: 'dSend', to: 'dsend' },
+  { id: 4, icon: dshop, title: 'dShop', to: 'dshop' },
+  { id: 5, icon: bloodlagbe, title: 'Blood Lagbe?', to: 'bloodlagbe' },
+  { id: 6, icon: gaslagbe, title: 'Gas Lagbe?', to: 'gaslagbe' },
   { id: 7, icon: delifood, title: 'delifood', to: 'delifood' },
 ];
 
