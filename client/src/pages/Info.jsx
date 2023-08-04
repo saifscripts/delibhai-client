c979509d704fcfa429051fbc0830992a374c332d
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2023-08-05 01:56:28 +0600

    add profile info

--- client/src/pages/Info.jsx
+++ client/src/pages/Info.jsx
@@ -2,9 +2,9 @@
 
 export default function Info({ info, type, icon }) {
   return (
-    <div className='flex gap-3 items-center border-b border-light pb-2'>
+    <div className='flex gap-3 items-center border-b border-light py-2'>
       <div className='w-12 aspect-square rounded-full bg-light flex justify-center items-center'>
-        <img src={icon} alt={type} className='w-[50%]' />
+        <img src={icon} alt={type} className='w-full' />
       </div>
       <div className='text-medium'>
         <p className='font-bold text-lg'>{info}</p>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,18 +9,81 @@ import InfoCategories from './InfoCategories';
 import InfoContainer from './InfoContainer';
 import Showcase from './Showcase';
 
-import user from '../assets/icons/user.svg';
+// icons
+import age from '../assets/icons/profile/age.svg';
+import bloodGroup from '../assets/icons/profile/blood-group.svg';
+import clock from '../assets/icons/profile/clock.svg';
+import email from '../assets/icons/profile/email.svg';
+import facebook from '../assets/icons/profile/facebook.svg';
+import father from '../assets/icons/profile/father.svg';
+import marker from '../assets/icons/profile/marker.svg';
+import nid from '../assets/icons/profile/NID.svg';
+import phone from '../assets/icons/profile/phone.svg';
+import serviceArea from '../assets/icons/profile/service-area.svg';
+import serviceType from '../assets/icons/profile/service-type.svg';
+import sex from '../assets/icons/profile/sex.svg';
+import station from '../assets/icons/profile/station.svg';
+import steering from '../assets/icons/profile/steering.svg';
+import user from '../assets/icons/profile/user.svg';
+import vehicleName from '../assets/icons/profile/vehicle--name.svg';
+import vehicleBrand from '../assets/icons/profile/vehicle-brand.svg';
+import vehicleModel from '../assets/icons/profile/vehicle-model.svg';
+import vehicleNumber from '../assets/icons/profile/vehicle-number.svg';
+import vehicleType from '../assets/icons/profile/vehicle.svg';
 
 const info = {
   personal: [
     { info: 'এয়াকুব বাবলু', type: 'নিজের নাম', icon: user },
-    { info: 'মোঃ ইউসুফ আলী', type: 'পিতার নাম', icon: user },
-    { info: 'পুরুষ', type: 'লিঙ্গ', icon: user },
-    { info: 'এ+', type: 'রক্তের গ্রুপ', icon: user },
-    { info: '২৯ বছর', type: 'বয়স', icon: user },
-    { info: '1023274465', type: 'জন্মনিবন্ধন/NID নম্বর', icon: user },
+    { info: 'মোঃ ইউসুফ আলী', type: 'পিতার নাম', icon: father },
+    { info: 'পুরুষ', type: 'লিঙ্গ', icon: sex },
+    { info: 'এ+', type: 'রক্তের গ্রুপ', icon: bloodGroup },
+    { info: '২৯ বছর', type: 'বয়স', icon: age },
+    { info: '1023274465', type: 'জন্মনিবন্ধন/NID নম্বর', icon: nid },
+  ],
+  contact: [
+    { info: 'এয়াকুব বাবলু', type: 'মোবাইল নম্বর', icon: phone },
+    { info: 'মোঃ ইউসুফ আলী', type: 'বিকল্প মোবাইল নম্বর', icon: phone },
+    { info: 'পুরুষ', type: 'ই-মেইল', icon: email },
+    { info: 'এ+', type: 'ফেইসবুক লিংক', icon: facebook },
+  ],
+  address: [
+    { info: 'এয়াকুব বাবলু', type: 'বর্তমান ঠিকানা', icon: marker },
+    { info: 'মোঃ ইউসুফ আলী', type: 'স্থায়ী ঠিকানা', icon: marker },
+  ],
+  vehicle: [
+    { info: 'এয়াকুব বাবলু', type: 'গাড়ির ধরণ', icon: vehicleType },
+    { info: 'মোঃ ইউসুফ আলী', type: 'গাড়ির ব্র্যান্ড', icon: vehicleBrand },
+    { info: 'পুরুষ', type: 'গাড়ির মডেল', icon: vehicleModel },
+    { info: 'এ+', type: 'গাড়ির নম্বর', icon: vehicleNumber },
+    { info: '২৯ বছর', type: 'গাড়ির নাম', icon: vehicleName },
+  ],
+  owner: [
+    { info: 'এয়াকুব বাবলু', type: 'কোম্পানির নাম', icon: user },
+    { info: 'মোঃ ইউসুফ আলী', type: 'ঠিকানা', icon: marker },
+    { info: 'পুরুষ', type: 'মোবাইল', icon: phone },
+    { info: 'এ+', type: 'ই-মেইল', icon: email },
+  ],
+  service: [
+    { info: 'এয়াকুব বাবলু', type: 'গাড়ির ব্যবহার', icon: steering },
+    { info: 'মোঃ ইউসুফ আলী', type: 'গাড়ির সেবা', icon: serviceType },
+    { info: 'পুরুষ', type: 'প্রধান স্ট্যাশন', icon: station },
+    { info: 'এ+', type: 'সার্ভিস প্রদানের এলাকা সমূহ', icon: serviceArea },
+    { info: 'এ+', type: 'সার্ভিস প্রদানের সময়', icon: clock },
+  ],
+  location: [
+    { info: 'এয়াকুব বাবলু', type: 'বিভাগ', icon: marker },
+    { info: 'মোঃ ইউসুফ আলী', type: 'জেলা', icon: marker },
+    { info: 'পুরুষ', type: 'উপজেলা/পৌরসভা', icon: marker },
+    { info: 'এ+', type: 'থানা', icon: marker },
+    { info: 'এ+', type: 'ইউনিয়ন/সিটি কর্পোরেশন', icon: marker },
+    { info: 'এ+', type: 'পোস্ট অফিস', icon: marker },
+    { info: 'এ+', type: 'পোস্ট কোড', icon: marker },
+    { info: 'এ+', type: 'গ্রাম/মহল্লা', icon: marker },
+    { info: 'এ+', type: 'স্ট্যাশন', icon: marker },
   ],
 };
+
+const { personal, contact, address, vehicle, owner, service, location } = info;
 
 export default function Profile() {
   const { id } = useParams();
@@ -49,19 +112,19 @@ export default function Profile() {
           {activeCategory === 1 && (
             <>
               <InfoContainer type='ব্যক্তিগত তথ্য'>
-                {info.personal.map(({ info, type, icon }, index) => (
+                {personal.map(({ info, type, icon }, index) => (
                   <Info key={index} info={info} type={type} icon={icon} />
                 ))}
               </InfoContainer>
 
               <InfoContainer type='কন্টাক্ট ইনফো'>
-                {info.personal.map(({ info, type, icon }, index) => (
+                {contact.map(({ info, type, icon }, index) => (
                   <Info key={index} info={info} type={type} icon={icon} />
                 ))}
               </InfoContainer>
 
               <InfoContainer type='ঠিকানা'>
-                {info.personal.map(({ info, type, icon }, index) => (
+                {address.map(({ info, type, icon }, index) => (
                   <Info key={index} info={info} type={type} icon={icon} />
                 ))}
               </InfoContainer>
@@ -71,13 +134,13 @@ export default function Profile() {
           {activeCategory === 2 && (
             <>
               <InfoContainer type='গাড়ির সাধারণ তথ্য'>
-                {info.personal.map(({ info, type, icon }, index) => (
+                {vehicle.map(({ info, type, icon }, index) => (
                   <Info key={index} info={info} type={type} icon={icon} />
                 ))}
               </InfoContainer>
 
               <InfoContainer type='গাড়ির মালিকানার তথ্য'>
-                {info.personal.map(({ info, type, icon }, index) => (
+                {owner.map(({ info, type, icon }, index) => (
                   <Info key={index} info={info} type={type} icon={icon} />
                 ))}
               </InfoContainer>
@@ -89,7 +152,7 @@ export default function Profile() {
           {activeCategory === 3 && (
             <>
               <InfoContainer type='সার্ভিস তথ্য'>
-                {info.personal.map(({ info, type, icon }, index) => (
+                {service.map(({ info, type, icon }, index) => (
                   <Info key={index} info={info} type={type} icon={icon} />
                 ))}
               </InfoContainer>
@@ -98,14 +161,10 @@ export default function Profile() {
 
           {activeCategory === 4 && (
             <>
-              <InfoContainer type='GPS Location'>
-                {info.personal.map(({ info, type, icon }, index) => (
-                  <Info key={index} info={info} type={type} icon={icon} />
-                ))}
-              </InfoContainer>
+              <InfoContainer type='GPS Location'></InfoContainer>
 
               <InfoContainer type='ম্যানুয়াল লোকেশন'>
-                {info.personal.map(({ info, type, icon }, index) => (
+                {location.map(({ info, type, icon }, index) => (
                   <Info key={index} info={info} type={type} icon={icon} />
                 ))}
               </InfoContainer>
