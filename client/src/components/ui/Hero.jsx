bc3c313a1238d309e224e5eec8229bcf17140d41
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2023-08-04 16:47:44 +0600

    resolved profile link share button propagation issue

--- client/src/components/ui/Hero.jsx
+++ client/src/components/ui/Hero.jsx
@@ -1,45 +1,33 @@
-import { BsTelephoneFill } from "react-icons/bs";
-import { GrLocation } from "react-icons/gr";
-import { PiPaperPlaneTiltFill } from "react-icons/pi";
-import { Link } from "react-router-dom";
-import PrimaryButton from "./PrimaryButton";
+import { BsTelephoneFill } from 'react-icons/bs';
+import { GrLocation } from 'react-icons/gr';
+import { PiPaperPlaneTiltFill } from 'react-icons/pi';
+import { Link } from 'react-router-dom';
+import PrimaryButton from './PrimaryButton';
 
 // eslint-disable-next-line react/prop-types
 export default function Hero({ details }) {
   // eslint-disable-next-line react/prop-types
   const { name, image, distance, id, mobile } = details;
 
-  const handleShareLink = async (e) => {
-    e.stopPropagation();
-
-    try {
-      await navigator.share({
-        link: `https://dev-delibhai.netlify.app/profile/${id}`,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   return (
     <Link to={`/profile/${id}`}>
-      <div className="flex items-center gap-5">
-        <div className="w-28 rounded-full">
-          <img src={image} alt="name" className="w-full rounded-full" />
+      <div className='flex items-center gap-5'>
+        <div className='w-28 rounded-full'>
+          <img src={image} alt='name' className='w-full rounded-full' />
         </div>
 
         <div>
-          <h3 className="text-xl mb-3">{name}</h3>
+          <h3 className='text-xl mb-3'>{name}</h3>
 
-          <div className="flex items-center gap-1 text-gray-700 mb-2">
+          <div className='flex items-center gap-1 text-gray-700 mb-2'>
             <span>{<GrLocation />}</span>
             <span>{distance}km</span>
           </div>
 
-          <div className="flex gap-3">
-            <div onClick={handleShareLink}>
+          <div className='flex gap-3'>
+            <Link onClick={(e) => e.stopPropagation()}>
               <PrimaryButton icon={<PiPaperPlaneTiltFill />} />
-            </div>
+            </Link>
 
             <Link to={`tel:${mobile}`} onClick={(e) => e.stopPropagation()}>
               <PrimaryButton icon={<BsTelephoneFill />} />
