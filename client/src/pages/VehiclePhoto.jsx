31e93a3eafb3058bb6acf24f8be04f82b401ceb2
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2023-08-05 18:08:13 +0600

    update vehicle photo border radius

--- client/src/pages/VehiclePhoto.jsx
+++ client/src/pages/VehiclePhoto.jsx
@@ -7,7 +7,7 @@ export default function VehiclePhoto({ url }) {
       style={{
         backgroundImage: `url(${url})`,
       }}
-      className={`relative group flex flex-col flex-shrink-0 justify-center items-center w-28 aspect-square p-1 bg-center bg-contain bg-no-repeat`}>
+      className={`relative group flex flex-col flex-shrink-0 justify-center items-center w-28 aspect-square p-1 bg-center bg-contain bg-no-repeat rounded-md overflow-hidden`}>
       <div className='absolute top-0 bottom-0 left-0 right-0 bg-black z-10 bg-opacity-40 hidden group-hover:flex group-active:flex justify-center items-center'>
         <img src={camera} alt='Camera' />
       </div>
