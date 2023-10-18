62a5ad3ecc3541048287103939b0d7b327b0e662
Md Saif Ullah <mdsaifullah1302@gmail.com>
2023-10-18 20:30:50 +0600

    use rest api to submit the signup form

--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -3,14 +3,14 @@
 function Input({ label, type, data, info, onInputChange, placeholder }) {
   return (
     <label>
-      <p className='font-bold mt-4 mb-1'>{label}</p>
+      <p className="font-bold mt-4 mb-1">{label}</p>
 
       <input
         type={type}
         value={data || info}
         placeholder={placeholder}
         onChange={onInputChange}
-        className='w-full py-3 px-2 border-b border-primary'
+        className="w-full py-3 border-b border-primary"
       />
     </label>
   );
--- src/features/Authentication/components/Signup.jsx
+++ src/features/Authentication/components/Signup.jsx
@@ -1,45 +1,45 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Input  from "../../../components/forms/Input";
+import Input from "../../../components/forms/Input";
 import SelectInput from "../../../components/forms/SelectInput";
 import Submit from "../../../components/forms/Submit";
 import PageContainer from "../../../layouts/PageContainer";
-import Title from "../../../layouts/Title"
-import TopPanel from "../../../layouts/TopPanel"
+import Title from "../../../layouts/Title";
+import TopPanel from "../../../layouts/TopPanel";
 
 const signupFields = [
   {
-    type: 'text',
-    label: 'পুরো নাম',
-    placeholder: 'পুরো নাম লিখুন',
+    type: "text",
+    label: "পুরো নাম",
+    placeholder: "পুরো নাম লিখুন",
   },
   {
-    type: 'select',
-    options: ['পুরুষ', 'মহিলা', 'অন্যান্য'],
-    data: 'পুরুষ',
-    label: 'লিঙ্গ',
+    type: "select",
+    options: ["পুরুষ", "মহিলা", "অন্যান্য"],
+    data: "পুরুষ",
+    label: "লিঙ্গ",
   },
   {
-    type: 'email',
-    label: 'ই-মেইল',
-    placeholder: 'ই-মেইল লিখুন',
+    type: "email",
+    label: "ই-মেইল",
+    placeholder: "ই-মেইল লিখুন",
   },
   {
-    type: 'number',
-    label: 'মোবাইল নাম্বার',
-    placeholder: 'মোবাইল নাম্বার লিখুন',
+    type: "text",
+    label: "মোবাইল নাম্বার",
+    placeholder: "মোবাইল নাম্বার লিখুন",
   },
   {
-    type: 'password',
-    label: 'পাসওয়ার্ড',
-    placeholder: 'পাসওয়ার্ড দিন',
+    type: "password",
+    label: "পাসওয়ার্ড",
+    placeholder: "পাসওয়ার্ড দিন",
   },
   {
-    type: 'password',
-    label: 'কনফার্ম পাসওয়ার্ড',
-    placeholder: 'পুনরায় পাসওয়ার্ড দিন',
+    type: "password",
+    label: "কনফার্ম পাসওয়ার্ড",
+    placeholder: "পুনরায় পাসওয়ার্ড দিন",
   },
-]
+];
 
 function Signup() {
   const [inputFields, setInputFields] = useState(signupFields);
@@ -51,55 +51,82 @@ function Signup() {
 
     clonedInputFields[i].data = e.target.value;
 
-    setInputFields(clonedInputFields)
-  }
+    setInputFields(clonedInputFields);
+  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
 
-    navigate('/otp-verification')
+      const userInfo = {
+        name: inputFields[0].data,
+        gender: inputFields[1].data,
+        email: inputFields[2].data,
+        mobile: inputFields[3].data,
+        password: inputFields[4].data,
+        confirmPassword: inputFields[5].data,
+      };
 
-  }
+      console.log(userInfo);
+      const response = await fetch("http://localhost:5000/api/v1/user/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userInfo),
+      });
+
+      const result = await response.json();
+
+      if (result.success) {
+        navigate("/otp-verification");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
       <TopPanel />
       <Title
-        title='একটি একাউন্ট তৈরী করুন'
-        subtitle='অনুগ্রহ করে সঠিক তথ্য দিয়ে একটি একাউন্ড তৈরী করুন'
+        title="একটি একাউন্ট তৈরী করুন"
+        subtitle="অনুগ্রহ করে সঠিক তথ্য দিয়ে একটি একাউন্ড তৈরী করুন"
       />
 
       <PageContainer>
         <form onSubmit={handleSubmit}>
-          {inputFields.map(({label, data, type, placeholder, options}, index) => {
-            if(type === 'select') {
+          {inputFields.map(
+            ({ label, data, type, placeholder, options }, index) => {
+              if (type === "select") {
+                return (
+                  <SelectInput
+                    key={label}
+                    label={label}
+                    onInputChange={(e) => onInputChange(e, index)}
+                    data={data || ""}
+                    options={options}
+                  />
+                );
+              }
+
               return (
-                <SelectInput
+                <Input
                   key={label}
                   label={label}
+                  data={data || ""}
+                  type={type}
+                  placeholder={placeholder}
                   onInputChange={(e) => onInputChange(e, index)}
-                  data={data || ''}
-                  options={options}
                 />
               );
             }
-
-            return <Input
-              key={label}
-              label={label}
-              data={data || ''}
-              type={type}
-              placeholder={placeholder}
-              onInputChange={(e) => onInputChange(e, index)}
-        />
-          }
-            
           )}
-          <Submit value='ওটিপি কোড পাঠান' />
+          <Submit value="ওটিপি কোড পাঠান" />
         </form>
       </PageContainer>
     </>
-  )
+  );
 }
 
-export {Signup};
+export { Signup };
