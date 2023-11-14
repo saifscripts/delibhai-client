f79b7fe4fd78e469f0de276be7778097046ed7b8
Md Saif Ullah <mdsaifullah1302@gmail.com>
2023-11-15 02:08:49 +0600

    capitalize error messages on signup page

--- src/features/Authentication/components/Login.jsx
+++ src/features/Authentication/components/Login.jsx
@@ -1,25 +1,23 @@
 import { useState } from "react";
 // import { useNavigate } from "react-router-dom";
-import Input  from "../../../components/forms/Input";
+import Input from "../../../components/forms/Input";
 import Submit from "../../../components/forms/Submit";
 import PageContainer from "../../../layouts/PageContainer";
-import Title from "../../../layouts/Title"
-import TopPanel from "../../../layouts/TopPanel"
+import Title from "../../../layouts/Title";
+import TopPanel from "../../../layouts/TopPanel";
 
 const loginFields = [
-  
   {
-    type: 'number',
-    label: 'মোবাইল নাম্বার',
-    placeholder: 'মোবাইল নাম্বার লিখুন',
+    type: "number",
+    label: "মোবাইল নাম্বার",
+    placeholder: "মোবাইল নাম্বার লিখুন",
   },
   {
-    type: 'password',
-    label: 'পাসওয়ার্ড',
-    placeholder: 'পাসওয়ার্ড দিন',
+    type: "password",
+    label: "পাসওয়ার্ড",
+    placeholder: "পাসওয়ার্ড দিন",
   },
-  
-]
+];
 
 function Login() {
   const [inputFields, setInputFields] = useState(loginFields);
@@ -31,41 +29,40 @@ function Login() {
 
     clonedInputFields[i].data = e.target.value;
 
-    setInputFields(clonedInputFields)
-  }
+    setInputFields(clonedInputFields);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // navigate('/')
-
-  }
+  };
 
   return (
     <>
       <TopPanel />
       <Title
-        title='লগিন করুন'
-        subtitle='সঠিক মোবাইল নাম্বার এবং পাসওয়ার্ড দিয়ে লগিন করুন'
+        title="লগিন করুন"
+        subtitle="সঠিক মোবাইল নাম্বার এবং পাসওয়ার্ড দিয়ে লগিন করুন"
       />
 
       <PageContainer>
         <form onSubmit={handleSubmit}>
-          {inputFields.map(({label, data, type, placeholder}, index) => 
-             <Input
-                key={label}
-                label={label}
-                data={data || ''}
-                type={type}
-                placeholder={placeholder}
-                onInputChange={(e) => onInputChange(e, index)}
-            />  
-          )}
-          <Submit value='লগিন' />
+          {inputFields.map(({ label, data, type, placeholder }, index) => (
+            <Input
+              key={label}
+              label={label}
+              data={data || ""}
+              type={type}
+              placeholder={placeholder}
+              onInputChange={(e) => onInputChange(e, index)}
+            />
+          ))}
+          <Submit value="লগিন" />
         </form>
       </PageContainer>
     </>
-  )
+  );
 }
 
-export {Login};
+export { Login };
