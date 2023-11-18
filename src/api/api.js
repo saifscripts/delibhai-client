278290470a678d99be6474e5eeb95f5b171e1789
Md Saif Ullah <mdsaifullah1302@gmail.com>
2023-11-18 17:08:00 +0600

    add useFetchData hook to fetch data

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,34 @@
 import { useState } from "react";
 import axios from "./axiosConfig";
 
+// useFetchData hook returns loading state and fetchData method
+export const useFetchData = () => {
+  // Loading State
+  const [isLoading, setIsLoading] = useState(false);
+
+  // fetchData method directly returns data/error
+  const fetchData = async (route) => {
+    let data, error;
+
+    try {
+      setIsLoading(true);
+      const response = await axios.get("/api" + route);
+      data = response.data;
+    } catch (err) {
+      console.log(err)
+      const appError = err?.response?.data;
+      const axiosError = err;
+      error = appError || axiosError;
+    } finally {
+      setIsLoading(false);
+    }
+
+    return { data, error };
+  };
+
+  return { isLoading, fetchData };
+};
+
 // usePostData hook returns loading state and postData method
 export const usePostData = () => {
   // Loading State
