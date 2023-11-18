2f76bf8ee8bf28f6d0b21f468f7a5876bc9e123a
Md Saif Ullah <mdsaifullah1302@gmail.com>
2023-11-18 22:30:46 +0600

    show currentUser name on Home component
    For testing purpose, the current user name will be
    shown on Home component in every auth state changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
-import Navbar from '../layouts/Navbar/Navbar';
+import { useAuth } from "../contexts/AuthContext";
+import Navbar from "../layouts/Navbar/Navbar";
 
 const Home = () => {
+  const { currentUser } = useAuth();
+  console.log(currentUser);
   return (
     <div>
       <Navbar />
+      <div className="flex justify-center items-center">
+        <p>{currentUser?.name}</p>
+      </div>
     </div>
   );
 };
