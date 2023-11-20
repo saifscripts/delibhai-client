086c20e620b6a402f3473535c5fc30f66bdd69f1
Md Saif Ullah <mdsaifullah1302@gmail.com>
2023-11-20 20:06:32 +0600

    add optional chaining operator to resolve error in profile page

--- src/features/Profile/hooks/useUserInfo.js
+++ src/features/Profile/hooks/useUserInfo.js
@@ -31,36 +31,34 @@ const useUserInfo = (id) => {
 
   useEffect(() => setUserInfo(heros.find((hero) => hero.id === id)), [id]);
 
-  const { presentAddress = {}, permanentAddress = {} } = userInfo;
-
   const modifiedInfo = {
     personal: [
-      { type: 'text', info: userInfo.name, label: 'নিজের নাম', icon: person },
-      { type: 'text', info: userInfo.father, label: 'পিতার নাম', icon: father },
+      { type: 'text', info: userInfo?.name, label: 'নিজের নাম', icon: person },
+      { type: 'text', info: userInfo?.father, label: 'পিতার নাম', icon: father },
       {
         type: 'select',
         options: ['পুরুষ', 'মহিলা', 'অন্যান্য'],
-        info: userInfo.gender,
+        info: userInfo?.gender,
         label: 'লিঙ্গ',
         icon: gender,
       },
       {
         type: 'select',
         options: ['A+', 'B+', 'AB+', 'O+', 'A-', 'B-', 'AB-', 'O-'],
-        info: userInfo.bloodGroup,
+        info: userInfo?.bloodGroup,
         label: 'রক্তের গ্রুপ',
         icon: bloodGroup,
       },
       {
         type: 'number',
-        data: userInfo.age,
-        info: `${convertNumber(userInfo.age)} বছর`,
+        data: userInfo?.age,
+        info: `${convertNumber(userInfo?.age)} বছর`,
         label: 'বয়স',
         icon: age,
       },
       {
         type: 'text',
-        info: userInfo.nid,
+        info: userInfo?.nid,
         label: 'জন্মনিবন্ধন/NID নম্বর',
         icon: nid,
       },
@@ -73,32 +71,32 @@ const useUserInfo = (id) => {
     contact: [
       {
         type: 'number',
-        info: userInfo.mobile1,
+        info: userInfo?.mobile1,
         label: 'মোবাইল নম্বর',
         icon: phone,
       },
       {
         type: 'number',
-        info: userInfo.mobile2,
+        info: userInfo?.mobile2,
         label: 'বিকল্প মোবাইল নম্বর',
         icon: phone,
       },
-      { type: 'email', info: userInfo.email, label: 'ই-মেইল', icon: email },
+      { type: 'email', info: userInfo?.email, label: 'ই-মেইল', icon: email },
       {
         type: 'text',
-        info: userInfo.facebook,
+        info: userInfo?.facebook,
         label: 'ফেইসবুক লিংক',
         icon: facebook,
       },
     ],
     address: [
       {
-        info: `${presentAddress.street}, ${presentAddress.post} - ${presentAddress.zip}, ${presentAddress.subDistrict}, ${presentAddress.district}।`,
+        info: `${userInfo?.presentAddress?.street}, ${userInfo?.presentAddress?.post} - ${userInfo?.presentAddress?.zip}, ${userInfo?.presentAddress?.subDistrict}, ${userInfo?.presentAddress?.district}।`,
         label: 'বর্তমান ঠিকানা',
         icon: marker,
       },
       {
-        info: `${permanentAddress.street}, ${permanentAddress.post} - ${permanentAddress.zip}, ${permanentAddress.subDistrict}, ${permanentAddress.district}।`,
+        info: `${userInfo?.permanentAddress?.street}, ${userInfo?.permanentAddress?.post} - ${userInfo?.permanentAddress?.zip}, ${userInfo?.permanentAddress?.subDistrict}, ${userInfo?.permanentAddress?.district}।`,
         label: 'স্থায়ী ঠিকানা',
         icon: marker,
       },
@@ -107,31 +105,31 @@ const useUserInfo = (id) => {
       {
         type: 'select',
         options: ['বাইক', 'কার', 'সিএনজি', 'টমটম'],
-        info: userInfo.vehicleType,
+        info: userInfo?.vehicleType,
         label: 'গাড়ির ধরণ',
         icon: vehicleType,
       },
       {
         type: 'text',
-        info: userInfo.vehicleBrand,
+        info: userInfo?.vehicleBrand,
         label: 'গাড়ির ব্র্যান্ড',
         icon: vehicleBrand,
       },
       {
         type: 'text',
-        info: userInfo.vehicleModel,
+        info: userInfo?.vehicleModel,
         label: 'গাড়ির মডেল',
         icon: vehicleModel,
       },
       {
         type: 'text',
-        info: userInfo.vehicleNumber,
+        info: userInfo?.vehicleNumber,
         label: 'গাড়ির নম্বর',
         icon: vehicleNumber,
       },
       {
         type: 'text',
-        info: userInfo.vehicleName,
+        info: userInfo?.vehicleName,
         label: 'গাড়ির নাম',
         icon: vehicleName,
       },
@@ -139,7 +137,7 @@ const useUserInfo = (id) => {
     owner: [
       {
         type: 'text',
-        info: userInfo.ownerName,
+        info: userInfo?.ownerName,
         label: 'কোম্পানি/মালিকের নাম',
         icon: person,
       },
@@ -193,40 +191,40 @@ const useUserInfo = (id) => {
         editOnly: true,
       },
       {
-        info: userInfo.ownerAddress,
+        info: userInfo?.ownerAddress,
         label: 'ঠিকানা',
         icon: marker,
         hidden: true,
       },
       {
         type: 'text',
-        info: userInfo.ownerMobile,
+        info: userInfo?.ownerMobile,
         label: 'মোবাইল',
         icon: phone,
       },
       {
         type: 'emeil',
-        info: userInfo.ownerEmail,
+        info: userInfo?.ownerEmail,
         label: 'ই-মেইল',
         icon: email,
       },
     ],
     service: [
-      { type: 'select', options:['ব্যক্তিগত', 'ভাড়ায় চালিত'], info: userInfo.serviceUsage, label: 'গাড়ির ব্যবহার', icon: steering },
-      { type: 'select',options:['লোকাল ভাড়া', 'রিজার্ভ ভাড়া', 'লোকাল ও রিজার্ভ ভাড়া', 'কন্টাক্ট ভাড়া'], info: userInfo.serviceType, label: 'গাড়ির সেবা', icon: serviceType },
+      { type: 'select', options:['ব্যক্তিগত', 'ভাড়ায় চালিত'], info: userInfo?.serviceUsage, label: 'গাড়ির ব্যবহার', icon: steering },
+      { type: 'select',options:['লোকাল ভাড়া', 'রিজার্ভ ভাড়া', 'লোকাল ও রিজার্ভ ভাড়া', 'কন্টাক্ট ভাড়া'], info: userInfo?.serviceType, label: 'গাড়ির সেবা', icon: serviceType },
       { type: 'category', label: 'সার্ভিস প্রদানের এলাকা', editOnly: true },
       
       {
         type: 'select',
         options: ['চট্টগ্রাম', 'ঢাকা'],
-        info: userInfo.serviceArea?.district,
+        info: userInfo?.serviceArea?.district,
         label: 'জেলা',
         editOnly: true,
       },
       {
         type: 'select',
         options: ['ফটিকছড়ি', 'হাটহাজারি'],
-        info: userInfo.serviceArea?.subDistrict,
+        info: userInfo?.serviceArea?.subDistrict,
         label: 'উপজেলা/পৌরসভা/সিটি কর্পোরেশন',
         editOnly: true,
       },
@@ -234,17 +232,17 @@ const useUserInfo = (id) => {
       {
         type: 'select',
         options: ['বিবিরহাট', 'হেয়াকো'],
-        info: userInfo.serviceArea?.post,
+        info: userInfo?.serviceArea?.post,
         label: 'পোস্ট অফিস',
         editOnly: true,
       },
       {
         type: 'number',
         label: 'পোস্ট কোড',
-        info: userInfo.serviceArea?.zip,
+        info: userInfo?.serviceArea?.zip,
         editOnly: true,
       },
-      { type: 'text', info: userInfo.mainStation, label: 'প্রধান স্ট্যাশন', icon: station },
+      { type: 'text', info: userInfo?.mainStation, label: 'প্রধান স্ট্যাশন', icon: station },
       {
         type: 'checkbox',
         options: [
@@ -257,22 +255,22 @@ const useUserInfo = (id) => {
           'পূর্ব পাড়া',
           'সরকার পাড়া'
         ],
-        info: userInfo.serviceArea?.areas?.join(', '),
-        data: userInfo.serviceArea?.areas,
+        info: userInfo?.serviceArea?.areas?.join(', '),
+        data: userInfo?.serviceArea?.areas,
         label: 'সার্ভিস প্রদানের এলাকা সমূহ',
         icon: serviceArea,
       },
       {
         type: 'time',
-        info: getServiceTime(userInfo.serviceTime),
-        data: userInfo.serviceTime,
+        info: getServiceTime(userInfo?.serviceTime),
+        data: userInfo?.serviceTime,
         label: 'সার্ভিস প্রদানের সময়',
         icon: clock,
       },
     ],
     location: [
       {
-        info: `${userInfo.location?.post}, ${userInfo.location?.thana}, ${userInfo.location?.subDistrict}, ${userInfo.location?.district}।`,
+        info: `${userInfo?.location?.post}, ${userInfo?.location?.thana}, ${userInfo?.location?.subDistrict}, ${userInfo?.location?.district}।`,
         label: 'ঠিকানা',
         icon: marker,
         hidden: true
@@ -280,48 +278,48 @@ const useUserInfo = (id) => {
       { 
         type: 'select',
         options: ['চট্টগ্রাম', 'ঢাকা'],
-        info: userInfo.location?.division, 
+        info: userInfo?.location?.division, 
         label: 'বিভাগ', 
         editOnly:true
       },
       { 
         type: 'select',
         options: ['চট্টগ্রাম', 'ঢাকা'],
-        info: userInfo.location?.district, 
+        info: userInfo?.location?.district, 
         label: 'জেলা', 
         editOnly:true
       },
       {
         type: 'select',
         options: ['চট্টগ্রাম', 'ঢাকা'],
-        info: userInfo.location?.subDistrict,
+        info: userInfo?.location?.subDistrict,
         label: 'উপজেলা/পৌরসভা/সিটি কর্পোরেশন',
         editOnly:true
       },
       {
         type: 'select',
         options: ['চট্টগ্রাম', 'ঢাকা'],
-        info: userInfo.location?.union,
+        info: userInfo?.location?.union,
         label: 'ইউনিয়ন',
         editOnly:true
       },
       { 
         type: 'select',
         options: ['চট্টগ্রাম', 'ঢাকা'],
-        info: userInfo.location?.post, 
+        info: userInfo?.location?.post, 
         label: 'পোস্ট অফিস', 
         editOnly:true
       },
       { 
         type: 'number',
-        info: userInfo.location?.zip, 
+        info: userInfo?.location?.zip, 
         label: 'পোস্ট কোড', 
         editOnly:true
       },
       { 
         type: 'select',
         options: ['চট্টগ্রাম', 'ঢাকা'],
-        info: userInfo.location?.station, 
+        info: userInfo?.location?.station, 
         label: 'স্ট্যাশন', 
         icon: station
       },
@@ -329,7 +327,7 @@ const useUserInfo = (id) => {
     videoURL: [
       {
         type: 'text',
-        info: userInfo.videoURL,
+        info: userInfo?.videoURL,
         label: 'ভিডিও লিংক',
         editOnly: true
       }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,11 @@
 // others
-import { useState } from 'react';
-import { AiFillStar } from 'react-icons/ai';
-import { CiLocationOn } from 'react-icons/ci';
-import ReactPlayer from 'react-player/youtube';
-import { useParams } from 'react-router-dom';
-import map from '../assets/map.png';
-import { vehicles } from '../features/DHero/index';
+import { useState } from "react";
+import { AiFillStar } from "react-icons/ai";
+import { CiLocationOn } from "react-icons/ci";
+import ReactPlayer from "react-player/youtube";
+import { useParams } from "react-router-dom";
+import map from "../assets/map.png";
+import { vehicles } from "../features/DHero/index";
 import {
   Achievement,
   AverageRating,
@@ -18,9 +18,9 @@ import {
   VehiclePhotos,
   useAddressFields,
   useUserInfo,
-} from '../features/Profile/index';
-import PageContainer from '../layouts/PageContainer';
-import TopPanel from '../layouts/TopPanel';
+} from "../features/Profile/index";
+import PageContainer from "../layouts/PageContainer";
+import TopPanel from "../layouts/TopPanel";
 
 export default function Profile() {
   const { id } = useParams();
@@ -28,9 +28,16 @@ export default function Profile() {
   const [userInfo, modifiedInfo] = useUserInfo(id);
   const addressFields = useAddressFields();
 
-  const { reviews } = userInfo;
-  const { personal, contact, address, vehicle, owner, service, location, videoURL } =
-    modifiedInfo;
+  const {
+    personal,
+    contact,
+    address,
+    vehicle,
+    owner,
+    service,
+    location,
+    videoURL,
+  } = modifiedInfo;
 
   return (
     <div>
@@ -41,11 +48,11 @@ export default function Profile() {
         {/* Showcase */}
         <Showcase
           id={id}
-          name={userInfo.name}
-          image={userInfo.image}
-          vehicle={userInfo.vehicleType}
+          name={userInfo?.name}
+          image={userInfo?.image}
+          vehicle={userInfo?.vehicleType}
           vehicleImage={
-            vehicles.find(({ title }) => title === userInfo.vehicleType)?.icon
+            vehicles.find(({ title }) => title === userInfo?.vehicleType)?.icon
           }
         />
 
@@ -58,7 +65,7 @@ export default function Profile() {
         {/* General Information */}
         {activeCategory === 1 && (
           <>
-            <InfoContainer type='ব্যক্তিগত তথ্য' fields={personal} edit>
+            <InfoContainer type="ব্যক্তিগত তথ্য" fields={personal} edit>
               {personal.map(
                 ({ info, label, icon, editOnly }, index) =>
                   editOnly || (
@@ -67,7 +74,7 @@ export default function Profile() {
               )}
             </InfoContainer>
 
-            <InfoContainer type='কন্টাক্ট ইনফো' fields={contact} edit>
+            <InfoContainer type="কন্টাক্ট ইনফো" fields={contact} edit>
               {contact.map(
                 ({ info, label, icon, editOnly }, index) =>
                   editOnly || (
@@ -76,7 +83,7 @@ export default function Profile() {
               )}
             </InfoContainer>
 
-            <InfoContainer type='ঠিকানা' fields={addressFields} edit>
+            <InfoContainer type="ঠিকানা" fields={addressFields} edit>
               {address.map(
                 ({ info, label, icon, editOnly }, index) =>
                   editOnly || (
@@ -90,7 +97,7 @@ export default function Profile() {
         {/* Vehicle Information */}
         {activeCategory === 2 && (
           <>
-            <InfoContainer type='গাড়ির সাধারণ তথ্য' fields={vehicle} edit>
+            <InfoContainer type="গাড়ির সাধারণ তথ্য" fields={vehicle} edit>
               {vehicle.map(
                 ({ info, label, icon, editOnly }, index) =>
                   editOnly || (
@@ -99,7 +106,7 @@ export default function Profile() {
               )}
             </InfoContainer>
 
-            <InfoContainer type='গাড়ির মালিকানার তথ্য' fields={owner} edit>
+            <InfoContainer type="গাড়ির মালিকানার তথ্য" fields={owner} edit>
               {owner.map(
                 ({ info, label, icon, editOnly }, index) =>
                   editOnly || (
@@ -108,7 +115,7 @@ export default function Profile() {
               )}
             </InfoContainer>
 
-            <InfoContainer type='গাড়ির ছবি'>
+            <InfoContainer type="গাড়ির ছবি">
               <VehiclePhotos photos={userInfo?.vehiclePhotos} />
             </InfoContainer>
           </>
@@ -117,7 +124,7 @@ export default function Profile() {
         {/* Service Information */}
         {activeCategory === 3 && (
           <>
-            <InfoContainer type='সার্ভিস তথ্য' fields={service} edit>
+            <InfoContainer type="সার্ভিস তথ্য" fields={service} edit>
               {service.map(
                 ({ info, label, icon, editOnly }, index) =>
                   editOnly || (
@@ -131,22 +138,22 @@ export default function Profile() {
         {/* Location Information */}
         {activeCategory === 4 && (
           <>
-            <InfoContainer type='GPS Location' edit>
-              <div className='p-3 flex justify-center items-center gap-2 border border-light rounded-md mb-3 text-medium'>
+            <InfoContainer type="GPS Location" edit>
+              <div className="p-3 flex justify-center items-center gap-2 border border-light rounded-md mb-3 text-medium">
                 <CiLocationOn />
                 <p>
-                  {'Heyako Bazar, Ramgarh Khagrachari Road, Fatikchhari, Chittagong, Bangladesh'.slice(
+                  {"Heyako Bazar, Ramgarh Khagrachari Road, Fatikchhari, Chittagong, Bangladesh".slice(
                     0,
                     30
-                  ) + ' . . .'}
+                  ) + " . . ."}
                 </p>
               </div>
-              <div className='border border-light rounded-md'>
-                <img src={map} alt='Map' className='w-full' />
+              <div className="border border-light rounded-md">
+                <img src={map} alt="Map" className="w-full" />
               </div>
             </InfoContainer>
 
-            <InfoContainer type='ম্যানুয়াল লোকেশন' fields={location} edit>
+            <InfoContainer type="ম্যানুয়াল লোকেশন" fields={location} edit>
               {location.map(
                 ({ info, label, icon, editOnly }, index) =>
                   editOnly || (
@@ -160,13 +167,13 @@ export default function Profile() {
         {/* Video */}
         {activeCategory === 5 && (
           <>
-            <InfoContainer type='ডিহিরোর অভিব্যক্তি' fields={videoURL} edit>
-              <div className='w-full max-w-lg aspect-[1/1.8] bg-black flex justify-center items-center rounded-lg overflow-hidden'>
+            <InfoContainer type="ডিহিরোর অভিব্যক্তি" fields={videoURL} edit>
+              <div className="w-full max-w-lg aspect-[1/1.8] bg-black flex justify-center items-center rounded-lg overflow-hidden">
                 <ReactPlayer
-                  width='100%'
-                  height='100%'
+                  width="100%"
+                  height="100%"
                   loop
-                  url={userInfo.videoURL}
+                  url={userInfo?.videoURL}
                 />
               </div>
             </InfoContainer>
@@ -176,28 +183,28 @@ export default function Profile() {
         {/* Review */}
         {activeCategory === 6 && (
           <>
-            <InfoContainer type='রাইডার এক্টিভিটি'>
-              <div className='flex gap-8 justify-center items-center py-3 border-b border-light'>
+            <InfoContainer type="রাইডার এক্টিভিটি">
+              <div className="flex gap-8 justify-center items-center py-3 border-b border-light">
                 <Achievement
-                  title='21K রিভিও'
-                  stat='4.3'
+                  title="21K রিভিও"
+                  stat="4.3"
                   icon={<AiFillStar />}
                 />
-                <Achievement title='রাইড শেয়ার' stat='30K' />
-                <Achievement title='রিসিভড কল' stat='35K' />
+                <Achievement title="রাইড শেয়ার" stat="30K" />
+                <Achievement title="রিসিভড কল" stat="35K" />
               </div>
             </InfoContainer>
 
-            <InfoContainer type='রেটিং এবং রিভিও'>
-              <div className='grid grid-cols-[1fr_3fr] gap-8 items-center px-2 py-3 border-b border-light'>
-                <AverageRating rating='3.7' total='21,000' />
+            <InfoContainer type="রেটিং এবং রিভিও">
+              <div className="grid grid-cols-[1fr_3fr] gap-8 items-center px-2 py-3 border-b border-light">
+                <AverageRating rating="3.7" total="21,000" />
                 <RatingBars percentages={[70, 20, 10, 3, 2]} />
               </div>
             </InfoContainer>
 
             <InfoContainer>
-              <div className='grid gap-8 px-2 py-3'>
-                {reviews?.map(
+              <div className="grid gap-8 px-2 py-3">
+                {userInfo?.reviews?.map(
                   ({ id, userImage, userName, rating, date, review }) => (
                     <Review
                       key={id}
