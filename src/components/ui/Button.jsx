32d60e6f02bafd75f1d7b0102b648072557aa011
Md Saif Ullah <mdsaifullah1302@gmail.com>
2023-11-29 22:20:01 +0600

    add style for disabled button on form submit button

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -2,10 +2,11 @@
 export default function Button({ value, icon, ...props }) {
   return (
     <button
-      className='bg-secondary text-white text-xl font-bold font-inter mx-auto w-full max-w-[310px] h-[70px] rounded-full shadow mt-6 flex justify-center items-center gap-5'
-      {...props}>
+      className="bg-secondary disabled:bg-slate-400 disabled:text-slate-600 text-white text-xl font-bold font-inter mx-auto w-full max-w-[310px] h-[70px] rounded-full shadow mt-6 flex justify-center items-center gap-5"
+      {...props}
+    >
       <span>{value}</span>
-      <span className='text-3xl'>{icon}</span>
+      <span className="text-3xl">{icon}</span>
     </button>
   );
 }
