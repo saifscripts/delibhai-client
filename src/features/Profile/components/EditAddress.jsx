615ab174bcbd2b5e4c22c2f6e7fa884e182cce5f
Md Saif Ullah <mdsaifullah1302@gmail.com>
2023-12-11 18:11:50 +0600

    feat(Profile): add permanent address on Edit

--- src/features/Profile/components/EditAddress.jsx
+++ src/features/Profile/components/EditAddress.jsx
@@ -1,73 +1,52 @@
 /* eslint-disable react/prop-types */
 import { getAllDivision } from "bd-divisions-to-unions";
-import * as _ from "lodash";
 import { useState } from "react";
 import Button from "../../../components/ui/Button";
 import PageContainer from "../../../layouts/PageContainer";
 import Title from "../../../layouts/Title";
 import TopPanel from "../../../layouts/TopPanel";
-import { getDistricts, getUnions, getUpazilas } from "../utils/getAddress";
+import EditAddressFields from "./EditAddressFields";
+import { RadioInput } from "./form/RadioInput";
 
 const EditAddress = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [showPermanentAddress, setShowPermanentAddress] = useState(false);
 
-  const [address, setAddress] = useState({
+  const [presentAddress, setPresentAddress] = useState({
     division: getAllDivision(),
     district: null,
     upazila: null,
     union: null,
   });
 
-  const handleChange = (e, currentField) => {
-    const currentValue = e.target.value;
-    const _address = _.cloneDeep(address);
-
-    // Make current value selected to show the selected value on UI
-    _address[currentField] = _address[currentField].map((fieldValue) => {
-      if (fieldValue.title === currentValue) {
-        fieldValue.selected = true;
-      } else {
-        fieldValue.selected = false;
-      }
-
-      return fieldValue;
-    });
-
-    // Update other lower level fields
-    switch (currentField) {
-      case "division":
-        _address.district = getDistricts(currentValue);
-        _address.upazila = null;
-        _address.union = null;
-        break;
-
-      case "district":
-        _address.upazila = getUpazilas(currentValue);
-        _address.union = null;
-        break;
-
-      case "upazila":
-        _address.union = getUnions(currentValue);
-        break;
-    }
-
-    setAddress(_address);
-  };
+  const [permanentAddress, setPermanentAddress] = useState({
+    division: getAllDivision(),
+    district: null,
+    upazila: null,
+    union: null,
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
 
-    const presentAddress = {};
+    const address = {
+      presentAddress: {},
+      permanentAddress: {},
+    };
 
     // Get selected field values
-    for (const field in address) {
-      presentAddress[field] = address[field]?.find(
+    for (const field in presentAddress) {
+      address.presentAddress[field] = presentAddress[field]?.find(
+        ({ selected }) => selected
+      )?.title;
+
+      address.permanentAddress[field] = permanentAddress[field]?.find(
         ({ selected }) => selected
       )?.title;
     }
 
-    console.log(presentAddress);
+    console.log(address);
 
     setIsLoading(false);
   };
@@ -76,7 +55,7 @@ const EditAddress = () => {
     <>
       <TopPanel />
       <Title
-        title="ব্যক্তিগত তথ্য"
+        title="ঠিকানা"
         subtitle="অনুগ্রহ করে সঠিক তথ্য দিয়ে একটি একাউন্ড তৈরী করুন"
       />
       <PageContainer>
@@ -85,73 +64,32 @@ const EditAddress = () => {
             বর্তমান ঠিকানা
           </p>
 
-          <div className="mt-4 mb-1">
-            <label className="font-bold">বিভাগ</label>
-            <select
-              disabled={isLoading}
-              value={address.division?.find(({ selected }) => selected)?.title}
-              className="w-full py-3 border-b border-primary bg-transparent"
-              onChange={(e) => handleChange(e, "division")}
-            >
-              <option>সিলেক্ট করুন</option>
-              {address.division?.map(({ title }) => (
-                <option key={title} value={title}>
-                  {title}
-                </option>
-              ))}
-            </select>
-          </div>
+          <EditAddressFields
+            address={presentAddress}
+            setAddress={setPresentAddress}
+          />
 
-          <div className="mt-4 mb-1">
-            <label className="font-bold">জেলা</label>
-            <select
-              value={address.district?.find(({ selected }) => selected)?.title}
-              disabled={isLoading}
-              className="w-full py-3 border-b border-primary bg-transparent"
-              onChange={(e) => handleChange(e, "district")}
-            >
-              <option>সিলেক্ট করুন</option>
-              {address.district?.map(({ title }) => (
-                <option key={title} value={title}>
-                  {title}
-                </option>
-              ))}
-            </select>
-          </div>
+          <p className="font-bold mt-4 mb-3 py-3 border-b border-light">
+            স্থায়ী ঠিকানা
+          </p>
 
-          <div className="mt-4 mb-1">
-            <label className="font-bold">উপজেলা</label>
-            <select
-              value={address.upazila?.find(({ selected }) => selected)?.title}
-              disabled={isLoading}
-              className="w-full py-3 border-b border-primary bg-transparent"
-              onChange={(e) => handleChange(e, "upazila")}
-            >
-              <option>সিলেক্ট করুন</option>
-              {address.upazila?.map(({ title }) => (
-                <option key={title} value={title}>
-                  {title}
-                </option>
-              ))}
-            </select>
-          </div>
+          <RadioInput
+            label="বর্তমান ও স্থায়ী ঠিকানা একই"
+            checked={!showPermanentAddress}
+            onClick={() => setShowPermanentAddress(false)}
+          />
+          <RadioInput
+            label="বর্তমান ও স্থায়ী ঠিকানা একই নয়"
+            checked={showPermanentAddress}
+            onClick={() => setShowPermanentAddress(true)}
+          />
 
-          <div className="mt-4 mb-1">
-            <label className="font-bold">ইউনিয়ন</label>
-            <select
-              value={address.union?.find(({ selected }) => selected)?.title}
-              disabled={isLoading}
-              className="w-full py-3 border-b border-primary bg-transparent"
-              onChange={(e) => handleChange(e, "union")}
-            >
-              <option>সিলেক্ট করুন</option>
-              {address.union?.map(({ title }) => (
-                <option key={title} value={title}>
-                  {title}
-                </option>
-              ))}
-            </select>
-          </div>
+          {showPermanentAddress && (
+            <EditAddressFields
+              address={permanentAddress}
+              setAddress={setPermanentAddress}
+            />
+          )}
 
           <Button disabled={isLoading} type="submit" value="সংরক্ষণ করুন" />
         </form>
