33c7e07d60d63aa2dc4941da2eca84d3c508c91f
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-01-30 22:30:22 +0600

    add new server link

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'http://localhost:5000',
-  baseURL: 'https://dev-delibhai.onrender.com', 
+  baseURL: 'https://delibhai-server.vercel.app/',
+  // baseURL: 'https://dev-delibhai.onrender.com', 
   timeout: 10000, 
   headers: {
     'Content-Type': 'application/json',
