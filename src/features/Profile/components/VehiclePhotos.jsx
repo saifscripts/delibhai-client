88baf2af740e33b82a85c00c1d205a5e067a4dbb
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-02-12 20:46:49 +0600

    limit vehicle image to 4 images

--- src/features/Profile/components/VehiclePhotos.jsx
+++ src/features/Profile/components/VehiclePhotos.jsx
@@ -14,15 +14,18 @@ export const VehiclePhotos = () => {
   const handleSubmit = async (event) => {
     setIsLoading(true);
 
+    // clone vehicle photo from the state
     let _vehiclePhotos = [];
     if (currentUser?.vehiclePhotos) {
       _vehiclePhotos = [...currentUser.vehiclePhotos];
     }
 
+    // create formData from the image
     let files = event.target.files;
     const formData = new FormData();
     formData.append("image", files[0]);
 
+    // upload image to the imgbb
     let response = await axios.post(
       `https://api.imgbb.com/1/upload?key=${
         import.meta.env.VITE_IMGBB_API_KEY
@@ -30,15 +33,18 @@ export const VehiclePhotos = () => {
       formData
     );
 
+    // return if upload is not success
     if (!response?.data?.success) {
       setIsLoading(false);
       return;
     }
 
+    // update the image url in database
     const { data, error } = await updateData(`/v1/user/${currentUser._id}`, {
       vehiclePhotos: [..._vehiclePhotos, response.data.data.url],
     });
 
+    // update current user state if database is updated
     if (data?.success) {
       setCurrentUser(data.data);
     } else {
@@ -61,20 +67,22 @@ export const VehiclePhotos = () => {
               setDeleteBtn={setDeleteBtn}
             />
           ))}
-          <form
-            className={`relative bg-accent z-10 w-28 aspect-square rounded-lg flex flex-col flex-shrink-0 justify-center items-center ${
-              isLoading && "opacity-30"
-            }`}
-          >
-            <img src={camera} alt="Camera" />
-            <input
-              type="file"
-              accept="image/*"
-              disabled={isLoading}
-              className="absolute top-0 bottom-0 right-0 left-0 z-20 opacity-0"
-              onChange={handleSubmit}
-            />
-          </form>
+          {currentUser?.vehiclePhotos?.length < 4 && (
+            <form
+              className={`relative bg-accent z-10 w-28 aspect-square rounded-lg flex flex-col flex-shrink-0 justify-center items-center ${
+                isLoading && "opacity-30"
+              }`}
+            >
+              <img src={camera} alt="Camera" />
+              <input
+                type="file"
+                accept="image/*"
+                disabled={isLoading}
+                className="absolute top-0 bottom-0 right-0 left-0 z-20 opacity-0"
+                onChange={handleSubmit}
+              />
+            </form>
+          )}
         </div>
       </div>
     </div>
