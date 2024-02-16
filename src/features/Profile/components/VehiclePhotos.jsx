ba7b5ba45eacbae5afa48fc0ab6d8241af03f9ea
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-02-16 10:58:36 +0600

    reset value when vehicle photo is selected

--- src/features/Profile/components/VehiclePhotos.jsx
+++ src/features/Profile/components/VehiclePhotos.jsx
@@ -75,6 +75,7 @@ export const VehiclePhotos = ({ userInfo }) => {
                 disabled={isLoading}
                 className="absolute top-0 bottom-0 right-0 left-0 z-20 opacity-0"
                 onChange={handleSubmit}
+                onClick={(e) => (e.target.value = null)} // reset value
               />
             </form>
           )}
