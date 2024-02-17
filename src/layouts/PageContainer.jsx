555f7ae39c69a9f7dc49d94fb25e4014187ffa84
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-02-17 22:50:08 +0600

    make page container responsive

--- src/layouts/PageContainer.jsx
+++ src/layouts/PageContainer.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line react/prop-types
 const PageContainer = ({ children }) => {
-  return <div className='container'>{children}</div>;
+  return <div className="container max-w-3xl shadow-xl py-4">{children}</div>;
 };
 
 export default PageContainer;
