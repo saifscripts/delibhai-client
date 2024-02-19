b7cdfd18f08ea0e1d96fdea5b9a7b2f79394ba38
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-02-19 15:04:28 +0600

    refactor english to bengali number converter utility func

---
+++ src/utils/englishToBengaliNumber.js
@@ -0,0 +1,8 @@
+function englishToBengaliNumber(number) {
+  const bengaliDigits = ["০", "১", "২", "৩", "৪", "৫", "৬", "৭", "৮", "৯"];
+  return String(number).replace(/[0-9]/g, function (match) {
+      return bengaliDigits[parseInt(match)];
+  });
+}
+
+export default englishToBengaliNumber;
