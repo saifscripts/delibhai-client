68b767856485e2149de0e8e8da66021e6a0d6964
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-02-21 12:32:55 +0600

    load userInfo from current user for self editing

--- src/features/Profile/hooks/useUserInfo.js
+++ src/features/Profile/hooks/useUserInfo.js
@@ -1,13 +1,20 @@
 import { useEffect, useState } from 'react';
 import { useFetchData } from '../../../api/api';
+import { useAuth } from '../../../contexts/AuthContext';
 
 const useUserInfo = (id) => {
   const [userInfo, setUserInfo] = useState({});
   const { isLoading, fetchData} = useFetchData();
+  const {currentUser} = useAuth();
 
   useEffect(() => {
-    fetchData(`/v1/user/${id}`).then(({data})=> setUserInfo(data.data))
-  }, [id]);
+    if (currentUser?._id === id) {
+      setUserInfo(currentUser)
+    } else {
+      fetchData(`/v1/user/${id}`).then(({data})=> setUserInfo(data.data))
+    }
+
+  }, [id, currentUser]);
 
   return {isLoading, userInfo};
 };
