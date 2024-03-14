e4493b3d51204da6c5ce140d625e19fee048a276
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-03-14 06:26:00 +0600

    update service category links

--- src/data/services.js
+++ src/data/services.js
@@ -8,14 +8,14 @@ import gaslagbe from '../assets/icons/gaslagbe.svg';
 import tutor from '../assets/icons/tutor.svg';
 
 const services = [
-  { id: 1, icon: dhero, title: 'dHero', to: 'dhero' },
-  { id: 2, icon: dlabour, title: 'dLabour', to: 'dlabour' },
-  { id: 3, icon: dsend, title: 'dSend', to: 'dsend' },
-  { id: 4, icon: dshop, title: 'dShop', to: 'dshop' },
-  { id: 5, icon: bloodlagbe, title: 'Blood Lagbe?', to: 'bloodlagbe' },
-  { id: 6, icon: gaslagbe, title: 'Gas Lagbe?', to: 'gaslagbe' },
-  { id: 7, icon: delifood, title: 'delifood', to: 'delifood' },
-  { id: 8, icon: tutor, title: 'Tutor', to: 'tutor' },
+  { id: 1, icon: dhero, title: 'dHero', subdomain: 'hero' },
+  { id: 2, icon: dlabour, title: 'dLabour', subdomain: 'labour' },
+  { id: 3, icon: dsend, title: 'dSend', subdomain: 'send' },
+  { id: 4, icon: dshop, title: 'dShop', subdomain: 'shop' },
+  { id: 5, icon: bloodlagbe, title: 'Blood Lagbe?', subdomain: 'blood' },
+  { id: 6, icon: gaslagbe, title: 'Gas Lagbe?', subdomain: 'gas' },
+  { id: 7, icon: delifood, title: 'delifood', subdomain: 'food' },
+  { id: 8, icon: tutor, title: 'Tutor', subdomain: 'tutor' },
 ];
 
 export default services;
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,8 +1,9 @@
 import services from "../data/services";
-import Categories from "../layouts/Categories";
 import Container from "../layouts/Container";
 import { Navbar } from "../layouts/Navbar/";
 import Title from "../layouts/Title";
+
+const host = location.host;
 
 const Services = () => {
   return (
@@ -10,7 +11,25 @@ const Services = () => {
       <Navbar bgColor="secondary" />
       <Container>
         <Title color="white" title="ডেলিভাই সার্ভিস সমূহ" />
-        <Categories categories={services} />
+        {/* Categories */}
+        <div className="grid grid-cols-2 gap-4 py-4 sm:gap-6 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 lg:gap-8">
+          {services.map(({ id, icon, title, subdomain }) => (
+            <a
+              href={subdomain + "." + host}
+              key={id}
+              className="bg-white rounded-lg aspect-square flex justify-center items-center shadow-md"
+            >
+              <div className="flex flex-col gap-4 lg:gap-6 justify-center items-center">
+                <img
+                  src={icon}
+                  alt={title}
+                  className="w-16 sm:w-20 lg:w-24 aspect-square"
+                />
+                <p className="text-xl sm:text-2xl text-center">{title}</p>
+              </div>
+            </a>
+          ))}
+        </div>
       </Container>
     </div>
   );
