ac21ee5a602f8c1afbc06d8ca53ee0933b2d6789
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-03-14 06:02:38 +0600

    use separate router for hero subdomain

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,11 @@
 import { Toaster } from "react-hot-toast";
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import { Login, OTPVerification, Signup } from "./features/Authentication";
-import Home from "./features/Home";
-import {
-  EditAddressInfo,
-  EditContactInfo,
-  EditManualLocationInfo,
-  EditPersonalInfo,
-  EditServiceInfo,
-  EditVideoURL,
-  Profile,
-} from "./features/Profile";
-import { EditOwnerInfo } from "./features/Profile/components/EditOwnerInfo";
-import { EditVehicleInfo } from "./features/Profile/components/EditVehicleInfo";
-import DHero from "./pages/DHero";
-import DHeroSearch from "./pages/DHeroSearch";
-import DHeroSearchResult from "./pages/DHeroSearchResult";
-import Services from "./pages/Services";
+import { RouterProvider } from "react-router-dom";
+import { getRouter } from "./Routers";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/services/dhero" element={<DHero />} />
-        <Route path="/services/dhero/:vehicle" element={<DHeroSearch />} />
-        <Route path="/services/dhero/search" element={<DHeroSearchResult />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/otp-verification" element={<OTPVerification />} />
-        <Route path="/profile/:id" element={<Profile />} />
-        <Route
-          path="/profile/edit/personal-info"
-          element={<EditPersonalInfo />}
-        />
-        <Route
-          path="/profile/edit/contact-info"
-          element={<EditContactInfo />}
-        />
-        <Route path="/profile/edit/address" element={<EditAddressInfo />} />
-        <Route
-          path="/profile/edit/vehicle-info"
-          element={<EditVehicleInfo />}
-        />
-        <Route path="/profile/edit/owner-info" element={<EditOwnerInfo />} />
-        <Route
-          path="/profile/edit/service-info"
-          element={<EditServiceInfo />}
-        />
-        <Route
-          path="/profile/edit/manual-location-info"
-          element={<EditManualLocationInfo />}
-        />
-        <Route path="/profile/edit/video" element={<EditVideoURL />} />
-      </Routes>
+      <RouterProvider router={getRouter()} />
       <Toaster />
     </>
   );
---
+++ src/Routers.jsx
@@ -0,0 +1,71 @@
+import { createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import { Login, OTPVerification, Signup } from "./features/Authentication";
+import Home from "./features/Home";
+import {
+  EditAddressInfo,
+  EditContactInfo,
+  EditManualLocationInfo,
+  EditPersonalInfo,
+  EditServiceInfo,
+  EditVideoURL,
+  Profile,
+} from "./features/Profile";
+import { EditOwnerInfo } from "./features/Profile/components/EditOwnerInfo";
+import { EditVehicleInfo } from "./features/Profile/components/EditVehicleInfo";
+import DHero from "./pages/DHero";
+import DHeroSearch from "./pages/DHeroSearch";
+import DHeroSearchResult from "./pages/DHeroSearchResult";
+import Services from "./pages/Services";
+
+const commonRoutes = [
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <Signup /> },
+  { path: "/otp-verification", element: <OTPVerification /> },
+  { path: "/profile/:id", element: <Profile /> },
+  {
+    path: "/profile/edit/personal-info",
+    element: <EditPersonalInfo />,
+  },
+  {
+    path: "/profile/edit/contact-info",
+    element: <EditContactInfo />,
+  },
+  { path: "/profile/edit/address", element: <EditAddressInfo /> },
+  {
+    path: "/profile/edit/vehicle-info",
+    element: <EditVehicleInfo />,
+  },
+  { path: "/profile/edit/owner-info", element: <EditOwnerInfo /> },
+  {
+    path: "/profile/edit/service-info",
+    element: <EditServiceInfo />,
+  },
+  {
+    path: "/profile/edit/manual-location-info",
+    element: <EditManualLocationInfo />,
+  },
+  { path: "/profile/edit/video", element: <EditVideoURL /> },
+];
+
+const appRouter = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/services", element: <Services /> },
+  ...commonRoutes,
+]);
+
+const dheroRouter = createBrowserRouter([
+  { path: "/", element: <DHero /> },
+  { path: "/:vehicle", element: <DHeroSearch /> },
+  { path: "/search", element: <DHeroSearchResult /> },
+  ...commonRoutes,
+]);
+
+export const getRouter = () => {
+  const subdomain = location.hostname.split(".")[0];
+  if (subdomain === "hero") {
+    return dheroRouter;
+  }
+
+  return appRouter;
+};
