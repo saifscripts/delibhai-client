87f624a30aff00088d2c476bb029ffb34a21ee46
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-03-14 08:38:40 +0600

    update service not found component

--- src/components/ServiceNotFound.jsx
+++ src/components/ServiceNotFound.jsx
@@ -1,17 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import Container from "../layouts/Container";
+
+const { host, protocol } = location;
+const domain = host.split(".").slice(1).join(".");
+
 function ServiceNotFound() {
-  const { host, protocol } = location;
-  const domain = host.split(".").slice(1).join(".");
+  const navigate = useNavigate();
 
   return (
-    <div className="min-h-[calc(100vh-4rem)] sm:min-h-[calc(100vh-6rem)] flex flex-col justify-center items-center">
-      <h1 className="text-red-400 text-7xl">Service Not Found</h1>
-      <a
-        href={`${protocol}//${domain}`}
-        className="bg-secondary px-8 py-3 mt-4 font-bold text-white rounded-md text-xl hover:bg-accent"
-      >
-        Go to Delibhai Home
-      </a>
-    </div>
+    <Container>
+      <div className="min-h-[calc(100vh-4rem)] sm:min-h-[calc(100vh-6rem)] flex flex-col justify-center items-center">
+        <h1 className="text-red-400 text-5xl lg:text-6xl text-center">
+          Service Not Found
+        </h1>
+        <div className="space-x-4 mt-6">
+          <button
+            onClick={() => navigate(-1)}
+            className="bg-secondary px-4 py-2 font-bold text-white rounded-md hover:bg-accent"
+          >
+            Back
+          </button>
+          <a
+            href={`${protocol}//${domain}`}
+            className="bg-secondary px-4 py-2 font-bold text-white rounded-md hover:bg-accent"
+          >
+            Go to Delibhai Home
+          </a>
+        </div>
+      </div>
+    </Container>
   );
 }
 
