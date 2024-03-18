7636506a8e6b0bea501d659cb9ecaec6093032ae
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-03-19 02:20:07 +0600

    refactor subdomain logic handler in a separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
 import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
 import { getRouter } from "./Routers";
-import services from "./data/services";
-
-window.addEventListener("message", function (event) {
-  // if (event.origin === "http://localhost:5173") {
-  if (event.data) {
-    console.log("message received in ", event.origin);
-    localStorage.setItem("authToken", event.data);
-  }
-  // }
-});
+import SubdomainStorageHandler from "./components/SubdomainStorageHandler";
 
 function App() {
   return (
     <>
       <RouterProvider router={getRouter()} />
-      <iframe
-        src={`https://delibhai.com`}
-        className="hidden"
-        id={`parent_frame`}
-      ></iframe>
-      {services?.map(
-        ({ id, subdomain, active }) =>
-          active && (
-            <iframe
-              key={id}
-              src={`https://${subdomain}.delibhai.com`}
-              className="hidden"
-              id={`${subdomain}_frame`}
-            ></iframe>
-          )
-      )}
+      <SubdomainStorageHandler />
       <Toaster />
     </>
   );
---
+++ src/components/SubdomainStorageHandler.jsx
@@ -0,0 +1,39 @@
+import services from "../data/services";
+
+window.addEventListener("message", function (event) {
+  if (event.origin !== window.location.origin) {
+    return;
+  }
+
+  if (event.data) {
+    localStorage.setItem("authToken", event.data);
+  }
+});
+
+function SubdomainStorageHandler() {
+  return (
+    <>
+      {/* Parent frame */}
+      <iframe
+        src={`https://delibhai.com`}
+        className="hidden"
+        id={`parent_frame`}
+      ></iframe>
+
+      {/* Subdomain frames */}
+      {services?.map(
+        ({ id, subdomain, active }) =>
+          active && (
+            <iframe
+              key={id}
+              src={`https://${subdomain}.delibhai.com`}
+              className="hidden"
+              id={`${subdomain}_frame`}
+            ></iframe>
+          )
+      )}
+    </>
+  );
+}
+
+export default SubdomainStorageHandler;
