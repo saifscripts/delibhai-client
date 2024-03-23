bff9dfe70dc3e6e3f4bbbb74fd77144fb9b68747
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-03-23 23:00:12 +0600

    update location data

--- src/features/DHero/components/GPSLocation.jsx
+++ src/features/DHero/components/GPSLocation.jsx
@@ -44,8 +44,10 @@ function GPSLocation({ setLocationType }) {
 
   return (
     <div className="flex aspect-video w-full flex-col items-center justify-center gap-2 rounded-lg border">
-      <p className="text-center text-2xl">Lat: {geoLocation?.latitude}</p>
-      <p className="text-center text-2xl">Lon: {geoLocation?.latitude}</p>
+      <p className="text-center text-2xl">Latitude: {geoLocation?.latitude}</p>
+      <p className="text-center text-2xl">
+        Longitude: {geoLocation?.longitude}
+      </p>
     </div>
   );
 }
