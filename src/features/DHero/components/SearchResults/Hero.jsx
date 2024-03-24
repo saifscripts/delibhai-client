842e93add241a1cd91f8076a298b4edfc2ee9462
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-03-24 16:11:17 +0600

    show if the hero is online

--- src/features/DHero/components/SearchResults/Hero.jsx
+++ src/features/DHero/components/SearchResults/Hero.jsx
@@ -1,43 +1,52 @@
 import { BsTelephoneFill } from "react-icons/bs";
 import { GrLocation } from "react-icons/gr";
 import { PiPaperPlaneTiltFill } from "react-icons/pi";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import dp from "../../../../assets/default.jpg";
 import PrimaryButton from "../../../../components/ui/PrimaryButton";
+import cn from "../../../../lib/cn.js";
 
-export default function Hero({ details }) {
-  const { name, avatarURL, _id, mobile, isOnline } = details;
+export default function Hero({ hero }) {
+  const { name, avatarURL, _id, mobile, isOnline, distance } = hero;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/profile/${_id}`);
+  };
 
   return (
-    <Link to={`/profile/${_id}`}>
-      <div className="flex items-center gap-5 rounded-lg p-4 hover:bg-neutral active:bg-neutral">
-        <div className="w-28 rounded-full">
-          <img
-            src={avatarURL || dp}
-            alt="name"
-            className="w-full rounded-full"
-          />
+    <div
+      onClick={handleClick}
+      className="flex items-center gap-5 rounded-lg p-4 hover:bg-neutral active:bg-neutral"
+    >
+      <div className="relative w-28 rounded-full">
+        <img src={avatarURL || dp} alt="name" className="w-full rounded-full" />
+        <div
+          className={cn(
+            "absolute bottom-[6px] right-[6px] aspect-square w-5 rounded-full border-2 border-white",
+            { "bg-primary": isOnline, "bg-accent": !isOnline },
+          )}
+        ></div>
+      </div>
+
+      <div>
+        <h3 className="mb-3 text-xl">{name}</h3>
+
+        <div className="mb-2 flex items-center gap-1 text-gray-700">
+          <span>{<GrLocation />}</span>
+          <span>{distance} km</span>
         </div>
 
-        <div>
-          <h3 className="mb-3 text-xl">{name}</h3>
+        <div className="flex gap-3">
+          <Link onClick={(e) => e.stopPropagation()}>
+            <PrimaryButton icon={<PiPaperPlaneTiltFill />} />
+          </Link>
 
-          <div className="mb-2 flex items-center gap-1 text-gray-700">
-            <span>{<GrLocation />}</span>
-            <span>{isOnline ? "online" : "offline"}</span>
-          </div>
-
-          <div className="flex gap-3">
-            <Link onClick={(e) => e.stopPropagation()}>
-              <PrimaryButton icon={<PiPaperPlaneTiltFill />} />
-            </Link>
-
-            <Link to={`tel:${mobile}`} onClick={(e) => e.stopPropagation()}>
-              <PrimaryButton icon={<BsTelephoneFill />} />
-            </Link>
-          </div>
+          <Link to={`tel:${mobile}`} onClick={(e) => e.stopPropagation()}>
+            <PrimaryButton icon={<BsTelephoneFill />} />
+          </Link>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
