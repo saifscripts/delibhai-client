75927815af7fe98138edbb057e4713a8696600c8
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-03-27 09:31:43 +0600

    add admin dashboard link on navbar

--- src/layouts/Navbar/components/Navbar.jsx
+++ src/layouts/Navbar/components/Navbar.jsx
@@ -1,13 +1,18 @@
 import { RxHamburgerMenu } from "react-icons/rx";
 import { Link } from "react-router-dom";
 import logo from "../../../assets/logos/logo.png";
+import { useAuth } from "../../../features/Authentication/contexts/AuthContext";
+import getLocation from "../../../utils/getLocation";
 import useSidebar from "../hooks/useSidebar";
 import { NavLinks } from "./NavLinks";
 import { Sidebar } from "./Sidebar";
 
+const { protocol, domain } = getLocation();
+
 // eslint-disable-next-line react/prop-types
 const Navbar = () => {
   const { isSidebarOpen, handleSidebarToggle } = useSidebar();
+  const { currentUser } = useAuth();
 
   return (
     <nav className="sticky top-0 z-50 bg-white lg:shadow-sm">
@@ -28,9 +33,20 @@ const Navbar = () => {
         />
 
         {/* Logo */}
-        <Link className="hidden lg:block" to={"/"}>
-          <img src={logo} alt="deliBhai Logo" className="w-28 sm:w-44" />
-        </Link>
+        <div className="flex items-center gap-10">
+          <Link className="hidden lg:block" to={"/"}>
+            <img src={logo} alt="deliBhai Logo" className="w-28 sm:w-44" />
+          </Link>
+
+          {currentUser?.role === "admin" && (
+            <a
+              href={`${protocol}//admin.${domain}`}
+              className="hidden bg-secondary px-6 py-3 font-semibold hover:brightness-90 lg:inline-block"
+            >
+              Dashboard
+            </a>
+          )}
+        </div>
 
         {/* Nav menu */}
         <ul className="hidden gap-2 lg:flex">
