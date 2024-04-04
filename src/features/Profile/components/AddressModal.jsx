e8bc0b3cf122d2af48612ffc4b27610af0db49d0
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-04-04 22:59:27 +0600

    feat(profile): display error for existing union on editing service area

--- src/features/Profile/components/AddressModal.jsx
+++ src/features/Profile/components/AddressModal.jsx
@@ -1,7 +1,9 @@
 import { cloneDeep } from "lodash";
+import { useEffect } from "react";
 import { AiFillPlusSquare } from "react-icons/ai";
 import MiniContainer from "../../../layouts/MiniContainer";
 import Modal from "../../../layouts/Modal";
+import { showErrorToast } from "../../../lib/toast";
 import { AddressFields } from "../../AddressFields";
 
 const AddressModal = ({
@@ -13,8 +15,26 @@ const AddressModal = ({
   setServiceAddress,
   addressIndex,
 }) => {
+  useEffect(() => {
+    const isUnionExist = serviceAddress?.some(
+      (add) => add?.union?._id === address?.union?._id,
+    );
+
+    if (addressIndex === serviceAddress.length && isUnionExist) {
+      return showErrorToast("ইউনিয়নটি ইতিমধ্যে যোগ করা হয়েছে!");
+    }
+  }, [address, serviceAddress, addressIndex]);
   const handleSave = (e) => {
     e.preventDefault();
+
+    const isUnionExist = serviceAddress?.some(
+      (add) => add?.union?._id === address?.union?._id,
+    );
+
+    if (addressIndex === serviceAddress.length && isUnionExist) {
+      return showErrorToast("ইউনিয়নটি ইতিমধ্যে যোগ করা হয়েছে!");
+    }
+
     const _serviceAddress = cloneDeep(serviceAddress);
     _serviceAddress[addressIndex] = address;
     setServiceAddress(_serviceAddress);
@@ -30,6 +50,7 @@ const AddressModal = ({
           address={address}
           setAddress={setAddress}
         />
+
         <button
           onClick={handleSave}
           className="flex w-full items-center justify-center gap-2 rounded-lg bg-primary px-2 py-2 text-xl text-white"
