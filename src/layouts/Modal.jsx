fd176a124880bf97b991ed66656c5ab8edf06f11
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-04-29 00:11:30 +0600

    fix: modal behavior

--- src/layouts/Modal.jsx
+++ src/layouts/Modal.jsx
@@ -1,17 +1,32 @@
-/* eslint-disable react/prop-types */
+import { useEffect } from "react";
 
 const Modal = ({ isOpen, onClose, className, children }) => {
-  if (!isOpen) {
-    return null;
-  }
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "visible";
+    }
+  }, [isOpen]);
+
+  const stopPropagation = (e) => {
+    e.stopPropagation();
+  };
+
+  if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center">
-      {/* Modal overlay */}
-      <div className="fixed inset-0 bg-black opacity-5" onClick={onClose}></div>
-      {/* Modal container */}
-      <div className={`z-50 rounded-lg bg-white shadow-lg ${className}`}>
-        {children}
+    <div
+      onClick={onClose}
+      className="fixed left-0 top-0 z-50 h-screen w-screen overflow-auto bg-black bg-opacity-5"
+    >
+      <div className="flex h-max min-h-screen w-full items-center justify-center">
+        <div
+          onClick={stopPropagation}
+          className={`m-10 overflow-hidden rounded-lg bg-white shadow-lg ${className}`}
+        >
+          {children}
+        </div>
       </div>
     </div>
   );
