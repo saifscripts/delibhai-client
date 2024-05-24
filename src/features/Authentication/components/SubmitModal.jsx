2aa757e9c8c6f6c41938f52098f0e04884b1a98b
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-05-24 18:30:46 +0600

    update submit modal

--- src/features/Authentication/components/SubmitModal.jsx
+++ src/features/Authentication/components/SubmitModal.jsx
@@ -6,17 +6,21 @@ import processing from "../assets/processing.png";
 function SubmitModal({ onClose, isOpen, id }) {
   return (
     <Modal
-      className="flex w-80 flex-col items-center gap-8 rounded-3xl py-9"
+      className="flex w-80 flex-col justify-center gap-8 rounded-3xl py-9"
       isOpen={isOpen}
       onClose={onClose}
     >
-      <img src={processing} alt="Processing User Registration" />
-      <div className="text-center">
-        <div className="text-xl text-primary">Successful</div>
+      <img
+        src={processing}
+        alt="Processing User Registration"
+        className="mx-auto mb-6 w-1/2"
+      />
+      <div className="mb-4 text-center">
+        <div className="mb-1 text-xl text-primary">Successful</div>
         <div className="w-60">Thank you for registering as dHero</div>
       </div>
       {/* <img src={loader} alt="Loading..." /> */}
-      <div className="flex gap-3">
+      <div className="flex justify-center gap-3">
         <Link to="/" className="rounded-md bg-primary px-3 py-1 text-white">
           হোম
         </Link>
