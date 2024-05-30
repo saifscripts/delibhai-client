17ed3685c1ef85793232d5164f18e9ee33e8a0f6
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-05-31 02:06:48 +0600

    provide local server access on local network

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0", // Allows external devices to connect
+    port: 5173, // or any port of your choice
+  },
   plugins: [react()],
 });
