7b71d42ba575532b90ef6dec2e6d1859e8fd1f4a
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-05-31 20:25:32 +0600

    remove console.log on location tracker error

--- src/components/LocationTracker.jsx
+++ src/components/LocationTracker.jsx
@@ -17,9 +17,7 @@ const LocationTracker = () => {
             liveLocation,
           });
         },
-        (error) => {
-          console.error("Error getting location:", error);
-        },
+        () => {},
       );
     } else {
       console.error("Geolocation is not supported by this browser.");
