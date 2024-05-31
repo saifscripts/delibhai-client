427f9120bb69ca597e08280d5ec81c7485148723
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-05-31 07:58:37 +0600

    navigate to login page after logout

--- src/features/Navbar/components/NavLinks.jsx
+++ src/features/Navbar/components/NavLinks.jsx
@@ -1,11 +1,14 @@
 import { AiFillHome, AiOutlineLogin, AiOutlineLogout } from "react-icons/ai";
 import { CgProfile } from "react-icons/cg";
 import { MdAppRegistration } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../features/Authentication/contexts/AuthContext";
 import CustomNavLink from "./CustomNavLink";
 
 const NavLinks = () => {
   const { currentUser, logout } = useAuth();
+  const navigate = useNavigate();
+
   return (
     <>
       <CustomNavLink to="/" icon={<AiFillHome />} text="হোম পেইজ" />
@@ -29,7 +32,10 @@ const NavLinks = () => {
       )}
       {currentUser && (
         <button
-          onClick={logout}
+          onClick={() => {
+            logout();
+            navigate("/login");
+          }}
           className="lg:blok flex items-center gap-5 px-8 py-4 text-sm hover:cursor-pointer active:bg-primary lg:rounded-lg lg:px-4 lg:py-4 lg:hover:bg-neutral"
         >
           <span className="text-2xl lg:hidden">
