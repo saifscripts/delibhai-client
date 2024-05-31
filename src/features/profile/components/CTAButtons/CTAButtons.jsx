5b1576995486bd8a410cf761ebf06291d6c991ea
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-06-01 00:02:37 +0600

    hide cta for self profile

--- src/features/profile/components/CTAButtons/CTAButtons.jsx
+++ src/features/profile/components/CTAButtons/CTAButtons.jsx
@@ -3,10 +3,16 @@ import { BsTelephoneFill } from "react-icons/bs";
 import { PiPaperPlaneTiltFill } from "react-icons/pi";
 import { Link } from "react-router-dom";
 import PrimaryButton from "../../../../components/ui/PrimaryButton";
+import { useAuth } from "../../../Authentication/contexts/AuthContext";
 import UserContext from "../../contexts/UserContext";
 
 export default function CTAButtons() {
   const { userInfo } = useContext(UserContext);
+  const { currentUser } = useAuth();
+
+  if (userInfo?._id === currentUser?._id) {
+    return;
+  }
 
   return (
     <div className="container fixed bottom-10 left-0 right-0 z-50 flex max-w-3xl items-center justify-center sm:bottom-2">
