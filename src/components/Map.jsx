27f82353b482f7446a68a56e35931fbb67c98148
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-06-01 07:12:28 +0600

    update map place holder

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,10 @@
 export default function Map({ geoLocation }) {
   return (
-    <div className="flex aspect-video w-full flex-col items-center justify-center gap-2 rounded-lg border">
+    <div className="flex w-full flex-col items-center justify-center gap-2 rounded-lg border py-5">
       {geoLocation?.latitude && geoLocation?.longitude ? (
-        <>
-          <p className="text-center text-2xl">
-            Latitude: {geoLocation?.latitude}
-          </p>
-          <p className="text-center text-2xl">
-            Longitude: {geoLocation?.longitude}
-          </p>
-        </>
+        <p className="text-center text-2xl">
+          {geoLocation?.latitude}, {geoLocation?.longitude}
+        </p>
       ) : (
         <p className="text-center text-lg text-red-400">Not Available</p>
       )}
