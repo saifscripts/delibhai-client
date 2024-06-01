75058de73beeef6ed51c8799f4da4439ff69077c
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-06-01 07:46:05 +0600

    update manual location edit form

--- src/features/profile/components/EditProfile/EditManualLocation.jsx
+++ src/features/profile/components/EditProfile/EditManualLocation.jsx
@@ -8,22 +8,17 @@ import { useAuth } from "../../../../features/Authentication/contexts/AuthContex
 import Modal from "../../../../layouts/Modal";
 
 const userSchema = yup.object({
-  latitude: yup
-    .number()
-    .typeError("Latitude must be a number")
-    .test(
-      "isValidLatitude",
-      `Invalid latitude!`,
-      (num) => isFinite(num) && Math.abs(num) <= 90,
-    ),
-  longitude: yup
-    .number()
-    .typeError("Longitude must be a number")
-    .test(
-      "isValidLongitude",
-      `Invalid longitude!`,
-      (num) => isFinite(num) && Math.abs(num) <= 180,
-    ),
+  geoLocation: yup
+    .string()
+    .test("isValidGeoLocation", `Invalid GeoLocation!`, (geo) => {
+      const [lat, lon] = geo.split(",").map((l) => l.trim());
+      return (
+        isFinite(lat) &&
+        Math.abs(lat) <= 90 &&
+        isFinite(lon) &&
+        Math.abs(lon) <= 180
+      );
+    }),
 });
 
 export default function EditManualLocation({ isOpen, onClose }) {
@@ -47,8 +42,7 @@ export default function EditManualLocation({ isOpen, onClose }) {
         navigator.geolocation.getCurrentPosition(
           (position) => {
             const { latitude, longitude } = position.coords;
-            setValue("latitude", latitude);
-            setValue("longitude", longitude);
+            setValue("geoLocation", `${latitude}, ${longitude}`);
           },
           (error) => {
             console.error("Error getting location:", error);
@@ -62,8 +56,12 @@ export default function EditManualLocation({ isOpen, onClose }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isOpen]);
 
-  const onSubmit = async (manualLocation) => {
+  const onSubmit = async ({ geoLocation }) => {
     setIsLoading(true);
+
+    const [latitude, longitude] = geoLocation.split(",").map((l) => l.trim());
+
+    const manualLocation = { latitude, longitude };
 
     // Update data
     const { data } = await updateData(`/v1/user/${currentUser._id}`, {
@@ -87,27 +85,15 @@ export default function EditManualLocation({ isOpen, onClose }) {
     >
       <form onSubmit={handleSubmit(onSubmit)} className="w-[512px] max-w-full">
         <div className="mb-1">
-          <label className="font-bold">Latitude</label>
+          <label className="font-bold">Geo Location</label>
           <input
             type="text"
             disabled={isLoading}
-            placeholder="Latitude"
-            {...register("latitude")}
+            placeholder="Geo Location"
+            {...register("geoLocation")}
             className="h-full w-full overflow-y-hidden border-b border-primary py-3"
           />
-          <p className="text-red-400">{errors.latitude?.message}</p>
-        </div>
-
-        <div className="mb-1">
-          <label className="font-bold">Longitude</label>
-          <input
-            type="text"
-            placeholder="Longitude"
-            disabled={isLoading}
-            {...register("longitude")}
-            className="h-full w-full overflow-y-hidden border-b border-primary py-3"
-          />
-          <p className="text-red-400">{errors.longitude?.message}</p>
+          <p className="text-red-400">{errors.geoLocation?.message}</p>
         </div>
 
         <Button disabled={isLoading} type="submit" value="সংরক্ষণ করুন" />
