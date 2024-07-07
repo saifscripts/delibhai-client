609bd0971994f6690c531081ae668ee00b773afc
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-07-07 06:30:55 +0600

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,4 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: "0.0.0.0", // Allows external devices to connect
-    port: 5173, // or any port of your choice
-  },
-  plugins: [react()],
-});
+export default defineConfig({ plugins: [react()] });
