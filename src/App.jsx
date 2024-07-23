7d25221c572d53c09f4a88ced6efa7b1a0626d3d
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-07-24 05:26:08 +0600

    use get me hook for fist load

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
+import { Fragment, useEffect } from "react";
 import { Toaster } from "react-hot-toast";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import LocationTracker from "./components/LocationTracker.jsx";
-import { getAuthUser } from "./redux/features/auth/authSlice.js";
+import { useGetMeQuery } from "./redux/features/auth/authApi.js";
+import { getAuthUser, setUser } from "./redux/features/auth/authSlice.js";
 import router from "./router.jsx";
 
 export default function App() {
   const user = useSelector(getAuthUser);
+  const dispatch = useDispatch();
+  const { data } = useGetMeQuery();
+
+  // update current user data on first load
+  useEffect(() => {
+    dispatch(setUser({ user: data?.data }));
+  }, [dispatch, data]);
 
   return (
-    <>
+    <Fragment>
       <RouterProvider router={router} />
       <Toaster />
       {user && (user.role === "rider" || user.role === "admin") && (
         <LocationTracker />
       )}
-    </>
+    </Fragment>
   );
 }
