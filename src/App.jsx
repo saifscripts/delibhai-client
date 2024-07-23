b8dc1efa9ccd577518dcffe96d13632eb1e2e6eb
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-07-23 10:43:13 +0600

    setup redux boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 import LocationTracker from "./components/LocationTracker.jsx";
 import { useAuth } from "./features/Authentication/contexts/AuthContext.jsx";
+import { persistor, store } from "./redux/store.js";
 import router from "./router.jsx";
 
 export default function App() {
   const { currentUser } = useAuth();
   return (
-    <>
-      <RouterProvider router={router} />
-      <Toaster />
-      {currentUser &&
-        (currentUser.role === "hero" || currentUser.role === "admin") && (
-          <LocationTracker />
-        )}
-    </>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+        <Toaster />
+        {currentUser &&
+          (currentUser.role === "hero" || currentUser.role === "admin") && (
+            <LocationTracker />
+          )}
+      </PersistGate>
+    </Provider>
   );
 }
---
+++ src/redux/api/baseApi.js
@@ -0,0 +1,40 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { logout, setUser } from "../features/auth/authSlice";
+import getRefreshToken from "./getRefreshToken";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: `${import.meta.env.VITE_BASE_URL2}/api/v1`,
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token;
+
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+
+    return headers;
+  },
+});
+
+const baseQueryWithRefreshToken = async (args, api, options) => {
+  let result = await baseQuery(args, api, options);
+
+  if (result?.error?.status === 401) {
+    const token = await getRefreshToken();
+    if (!token) api.dispatch(logout());
+
+    const user = api.getState().auth.user;
+
+    api.dispatch(setUser({ user, token }));
+
+    result = await baseQuery(args, api, options);
+  }
+
+  return result;
+};
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: baseQueryWithRefreshToken,
+  endpoints: () => ({}),
+});
