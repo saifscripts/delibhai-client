5bd89c93ae7b6a242834b119f0f29e022503b0a3
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-07-24 04:59:47 +0600

    remove auth context

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.jsx";
-import { AuthProvider } from "./features/Authentication/contexts/AuthContext.jsx";
 import "./index.css";
 import { persistor, store } from "./redux/store.js";
 
@@ -11,9 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>,
