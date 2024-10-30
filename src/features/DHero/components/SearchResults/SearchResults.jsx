9095e65afe382f8f3875a9fc634d6711c13c9c5b
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-10-30 14:10:58 +0600

    update search rider api

--- src/features/DHero/components/SearchResults/SearchResults.jsx
+++ src/features/DHero/components/SearchResults/SearchResults.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import { useFetchData } from "../../../../api/api";
 import Container from "../../../../layouts/Container";
+import axios from "../../../../lib/api";
 import { isCurrentTimeWithinServiceTimes } from "../../../../utils/timeHelpers";
 import { getDistance } from "../../utils/getDistance";
 import Hero from "./Hero";
@@ -9,7 +9,6 @@ import Hero from "./Hero";
 export default function SearchResults() {
   const [heros, setHeros] = useState([]);
   const [searchParams] = useSearchParams();
-  const { fetchData } = useFetchData();
 
   useEffect(() => {
     const _searchParams = Object.fromEntries([...searchParams]);
@@ -26,45 +25,49 @@ export default function SearchResults() {
             longitude: undefined,
           };
 
-    fetchData("/v1/user/heros", {
-      vehicle,
-      destination,
-    }).then((data) => {
-      if (data?.data?.success) {
-        let heros = data.data.data;
+    axios
+      .get("/rider", {
+        params: {
+          vehicle,
+          destination,
+        },
+      })
+      .then((data) => {
+        if (data?.data?.success) {
+          let heros = data.data.data;
 
-        heros = heros.map((hero) => {
-          const heroCurrentLocation =
-            hero?.liveLocation || hero?.manualLocation;
+          heros = heros.map((hero) => {
+            const heroCurrentLocation =
+              hero?.liveLocation || hero?.manualLocation;
 
-          return {
-            ...hero,
-            isHighlight:
-              hero?.mainStation?.village?._id === destination?.village?._id, // highlight if destination === main station
-            isOnline:
-              hero.serviceStatus === "off"
-                ? false
-                : hero.serviceStatus === "on"
-                  ? true
-                  : isCurrentTimeWithinServiceTimes(hero.serviceTimes),
-            currentDistance: getDistance(
-              userCurrentLocation,
-              heroCurrentLocation,
-            ),
-          };
-        });
+            return {
+              ...hero,
+              isHighlight:
+                hero?.mainStation?.village?._id === destination?.village?._id, // highlight if destination === main station
+              isOnline:
+                hero.serviceStatus === "off"
+                  ? false
+                  : hero.serviceStatus === "on"
+                    ? true
+                    : isCurrentTimeWithinServiceTimes(hero.serviceTimes),
+              currentDistance: getDistance(
+                userCurrentLocation,
+                heroCurrentLocation,
+              ),
+            };
+          });
 
-        heros.sort(function (a, b) {
-          if (!a.isOnline && b.isOnline) return 1;
-          if (a.isOnline && !b.isOnline) return -1;
+          heros.sort(function (a, b) {
+            if (!a.isOnline && b.isOnline) return 1;
+            if (a.isOnline && !b.isOnline) return -1;
 
-          if (a.currentDistance - b.currentDistance > 0) return 1;
-          if (a.currentDistance - b.currentDistance < 0) return -1;
-        });
+            if (a.currentDistance - b.currentDistance > 0) return 1;
+            if (a.currentDistance - b.currentDistance < 0) return -1;
+          });
 
-        setHeros(heros);
-      }
-    });
+          setHeros(heros);
+        }
+      });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchParams]);
 
