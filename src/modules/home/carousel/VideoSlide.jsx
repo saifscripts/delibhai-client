505c99d9715c74ff539e2383574c8db8b2fc519b
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-11-19 18:37:47 +0600

    add comments

--- src/modules/home/carousel/VideoSlide.jsx
+++ src/modules/home/carousel/VideoSlide.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ReactPlayer from "react-player/lazy";
 
 export default function VideoSlide({ slide }) {
+  // Controlled playback (to enable toggle play/pause over the overlay layer)
   const [playing, setPlaying] = useState(false);
 
   return (
@@ -17,6 +18,7 @@ export default function VideoSlide({ slide }) {
         loop
         url={slide.videoURL}
       />
+      {/* Overlay (to enable sliding over the video) */}
       <div className="absolute left-0 right-0 top-0 z-10 h-[calc(100%-56px)] cursor-pointer bg-transparent" />
     </div>
   );
