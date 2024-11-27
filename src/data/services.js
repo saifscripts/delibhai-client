f2a2ebe47b96db6eed58468b87178c898d8c6bf3
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-11-27 21:03:42 +0600

    update services name

--- src/data/services.js
+++ src/data/services.js
@@ -4,6 +4,7 @@ import delifood from "../assets/icons/delifood.svg";
 import dlabour from "../assets/icons/dlabour.svg";
 import dsend from "../assets/icons/dsend.svg";
 import dshop from "../assets/icons/dshop.svg";
+import home from "../assets/icons/home.svg";
 import it from "../assets/icons/it.svg";
 import tutor from "../assets/icons/tutor.svg";
 import getUserAgent from "../utils/getUserAgent";
@@ -11,14 +12,15 @@ import getUserAgent from "../utils/getUserAgent";
 const userAgent = getUserAgent();
 
 const services = [
-  { id: 1, icon: dhero, title: "বাহন", to: "vehicles", active: true },
-  { id: 2, icon: dlabour, title: "লেবার", to: "labour" },
-  { id: 3, icon: tutor, title: "টিউটর", to: "tutor" },
-  { id: 4, icon: bloodlagbe, title: "ব্লাড লাগবে", to: "blood" },
-  { id: 5, icon: dsend, title: "পার্সেল", to: "send" },
-  { id: 6, icon: dshop, title: "শপ", to: "shop" },
+  { id: 1, icon: dhero, title: "গাড়ী লাগবে", to: "vehicles", active: true },
+  { id: 2, icon: dlabour, title: "লেবার লাগবে", to: "labour" },
+  { id: 3, icon: tutor, title: "টিউটর লাগবে", to: "tutor" },
+  { id: 4, icon: home, title: "বাসা লাগবে", to: "house" },
+  { id: 5, icon: dshop, title: "শপিং করুন", to: "shop" },
+  { id: 6, icon: dsend, title: "পার্সেল পাঠান", to: "send" },
+  { id: 7, icon: bloodlagbe, title: "ব্লাড লাগবে", to: "blood" },
   {
-    id: 7,
+    id: 8,
     icon: it,
     title: "ডেলিভাই আইটি",
     to:
@@ -29,7 +31,7 @@ const services = [
           : "fb://profile/127914237079370",
   },
   {
-    id: 8,
+    id: 9,
     icon: delifood,
     title: "ডেলিফুড",
     to:
