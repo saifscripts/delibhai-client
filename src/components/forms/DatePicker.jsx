e4ff2271e9472577d0681ed44543b9cafcec90e2
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-12-18 18:39:07 +0600

    update datepicker icon and color

--- src/components/forms/DatePicker.jsx
+++ src/components/forms/DatePicker.jsx
@@ -6,6 +6,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { cn } from "@/lib/utils";
+import { CalendarDays } from "lucide-react";
 import { useFormContext } from "react-hook-form";
 
 export default function DatePicker({ name, label, placeholder, className }) {
@@ -21,18 +22,21 @@ export default function DatePicker({ name, label, placeholder, className }) {
             <FormLabel className="text-base font-bold">{label}</FormLabel>
           )}
           <FormControl>
-            <input
-              type="date"
-              placeholder={placeholder}
-              {...field}
-              className={cn(
-                "block w-full border-b border-t-0 border-primary px-0 py-2 focus-visible:ring-0",
-                className,
-                {
-                  "border-destructive/80": !!form.formState.errors[name],
-                },
-              )}
-            />
+            <div className="relative">
+              <input
+                type="date"
+                placeholder={placeholder}
+                {...field}
+                className={cn(
+                  "block w-full border-b border-t-0 border-primary bg-transparent px-0 py-2 text-foreground focus-visible:ring-0",
+                  className,
+                  {
+                    "border-destructive/80": !!form.formState.errors[name],
+                  },
+                )}
+              />
+              <CalendarDays className="absolute right-0 top-1/2 -z-10 -translate-y-1/2 cursor-pointer text-foreground/70" />
+            </div>
           </FormControl>
           <FormMessage />
         </FormItem>
