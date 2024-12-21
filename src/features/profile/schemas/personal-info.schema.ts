7f7554e53eb9dc9502d153d3205d15490e412a47
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-12-22 01:05:07 +0600

    [profile/personal-info]: update validation schema

--- src/features/profile/schemas/personal-info.schema.ts
+++ src/features/profile/schemas/personal-info.schema.ts
@@ -1,44 +1,62 @@
-import { isNID } from "@/utils/isNID";
-import { z } from "zod";
+import { isNID } from '@/utils/isNID';
+import { z } from 'zod';
 
 export const PersonalInfoSchema = z.object({
   name: z
-    .string({ required_error: "Name is required!" })
+    .string({ required_error: 'Name is required!' })
     .trim()
-    .min(1, "Name is required!")
-    .min(3, "Name must be at least 3 characters long!"),
+    .min(1, 'Name is required!')
+    .min(3, 'Name must be at least 3 characters long!'),
   fatherName: z
     .string()
     .trim()
-    .transform((val) => (val === "" ? null : val))
+    .transform((val) => (val === '' ? null : val))
     .refine((value) => value === null || value.length > 3, {
       message: "Father's name must be at least 3 characters long!",
     })
     .nullable()
     .optional(),
   gender: z
-    .enum(["পুরুষ", "মহিলা", "অন্যান্য"], {
-      invalid_type_error: "Gender must be পুরুষ/মহিলা/অন্যান্য!",
-    })
+    .string()
+    .transform((val) => (val === '' ? null : val))
+    .refine(
+      (val) =>
+        val === null ||
+        z.enum(['পুরুষ', 'মহিলা', 'অন্যান্য']).safeParse(val).success,
+      {
+        message: 'Gender must be পুরুষ/মহিলা/অন্যান্য!',
+      }
+    )
+    .nullable()
     .optional(),
   bloodGroup: z
-    .enum(["এ+", "বি+", "এবি+", "ও+", "এ-", "বি-", "এবি-", "ও-"], {
-      invalid_type_error: "Invalid blood group!",
-    })
+    .string()
+    .transform((val) => (val === '' ? null : val))
+    .refine(
+      (val) =>
+        val === null ||
+        z
+          .enum(['এ+', 'বি+', 'এবি+', 'ও+', 'এ-', 'বি-', 'এবি-', 'ও-'])
+          .safeParse(val).success,
+      {
+        message: 'Invalid blood group!',
+      }
+    )
+    .nullable()
     .optional(),
   dateOfBirth: z
     .string()
-    .transform((val) => (val === "" ? null : val))
+    .transform((val) => (val === '' ? null : val))
     .refine((val) => val === null || z.string().date().safeParse(val).success, {
-      message: "Invalid date of birth!",
+      message: 'Invalid date of birth!',
     })
     .nullable()
     .optional(),
   nid: z
     .string()
-    .transform((val) => (val === "" ? null : val))
+    .transform((val) => (val === '' ? null : val))
     .refine((val) => val === null || isNID(val), {
-      message: "Invalid NID number!",
+      message: 'Invalid NID number!',
     })
     .nullable()
     .optional(),
