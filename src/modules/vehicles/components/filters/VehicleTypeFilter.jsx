1424fd08cd89523afaea9678b443a309da578dbb
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-12-28 03:52:30 +0600

    [vehicle/search]: remove vehicle name from vehicle type filter

--- src/modules/vehicles/components/filters/VehicleTypeFilter.jsx
+++ src/modules/vehicles/components/filters/VehicleTypeFilter.jsx
@@ -24,22 +24,22 @@ export default function VehicleTypeFilter() {
               key={title}
               onClick={() => handleChangeVehicleType(title)}
               className={cn(
-                'group flex aspect-square cursor-pointer flex-col items-center justify-between gap-1 rounded-lg border bg-muted p-1 hover:border-tone-600 hover:bg-tone-200 size-16',
+                'group flex aspect-square cursor-pointer flex-col items-center justify-center gap-1 rounded-lg border bg-muted p-2 hover:border-tone-600 hover:bg-tone-200 size-16',
                 {
                   'bg-tone-200': vehicleType === title,
                 }
               )}
             >
-              <div className="flex h-[50%] w-full items-center justify-center">
+              <div className="flex h-full w-full items-center justify-center">
                 <img
                   src={icon}
                   alt={title}
-                  className="h-full w-full object-scale-down object-center"
+                  className="h-full w-full object-contain object-center"
                 />
               </div>
-              <p className="text-center text-muted-foreground [text-wrap:nowrap] dark:group-hover:text-muted text-[12px]">
+              {/* <p className="text-center text-muted-foreground [text-wrap:nowrap] dark:group-hover:text-muted text-[12px]">
                 {title}
-              </p>
+              </p> */}
             </ScrollItem>
           ))}
         </ScrollMenu>
