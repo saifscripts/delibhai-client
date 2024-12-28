ff57c6c879c61f175c55fd8eb07f4084e28e1b7f
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-12-28 23:45:06 +0600

    [vehicle]: add save button draft

--- src/features/DHero/components/SearchResults/RiderCard.jsx
+++ src/features/DHero/components/SearchResults/RiderCard.jsx
@@ -1,7 +1,14 @@
 import { Button } from '@/components/ui/button';
+import vehicles from '@/data/vehicles.js';
 import Container from '@/layouts/Container.jsx';
-import { MapPin, Phone } from 'lucide-react';
-import { BsWhatsapp } from 'react-icons/bs';
+import { MapPin } from 'lucide-react';
+import { useState } from 'react';
+import {
+  PiPhoneFill,
+  PiStar,
+  PiStarFill,
+  PiWhatsappLogoFill,
+} from 'react-icons/pi';
 import { Link, useNavigate } from 'react-router-dom';
 import dp from '../../../../assets/default.jpg';
 import cn from '../../../../lib/cn.js';
@@ -12,8 +19,19 @@ import stationIcon from './station.png';
 
 export default function RiderCard({ rider }) {
   const { _id, name, avatarURL, isOnline, distance } = rider;
+  const [isSaved, setIsSaved] = useState(false);
 
   rider.isHighlight = false; // TODO: remove this when we have a real data
+
+  const vehicleType = vehicles.find(
+    ({ title }) => title === rider?.vehicleType
+  );
+
+  const vehicleSubTypes = vehicleType?.subTypes;
+
+  const vehicleIcon = rider?.vehicleSubType
+    ? vehicleSubTypes.find(({ title }) => title === rider?.vehicleSubType)?.icon
+    : vehicleType?.icon;
 
   const navigate = useNavigate();
 
@@ -25,10 +43,10 @@ export default function RiderCard({ rider }) {
     <Container>
       <div
         onClick={handleClick}
-        className="bg-background hover:bg-tone-400/20 active:bg-tone-400/20 shadow-md rounded-lg p-4"
+        className="bg-background hover:bg-tone-400/20 active:bg-tone-400/20 shadow-sm rounded-lg p-4"
       >
         <div className="flex cursor-pointer items-center gap-5 rounded-lg">
-          <div className="relative aspect-square w-24 rounded-full">
+          <div className="relative aspect-square w-20 rounded-full">
             <img
               src={avatarURL || dp}
               alt="name"
@@ -43,78 +61,119 @@ export default function RiderCard({ rider }) {
           </div>
 
           <div className="flex-1">
-            <h3 className="text-xl font-bold">{name}</h3>
+            <div className="flex gap-4 justify-between">
+              <div>
+                <h3 className="text-base font-bold">{name}</h3>
+                <div className="flex items-center gap-1 text-xs">
+                  <img src={rentIcon} alt="" />
+                  <span>
+                    {rider?.rentType?.map((type) => `${type}  ভাড়া`).join(', ')}
+                  </span>
+                </div>
 
-            <div className="flex items-center gap-1 text-sm">
-              <img src={rentIcon} alt="" />
-              <span>
-                {rider?.rentType?.map((type) => `${type}  ভাড়া`).join(', ')}
-              </span>
-            </div>
+                {rider?.mainStation && (
+                  <div className="flex items-center gap-1 text-xs">
+                    <img
+                      src={rider.isHighlight ? stationActiveIcon : stationIcon}
+                      alt=""
+                    />
+                    <span
+                      className={cn({
+                        'text-destructive': rider.isHighlight,
+                      })}
+                    >
+                      {getVillageTitle(rider.mainStation)}
+                    </span>
+                  </div>
+                )}
 
-            {rider?.mainStation && (
-              <div className="flex items-center gap-1 text-sm">
-                <img
-                  src={rider.isHighlight ? stationActiveIcon : stationIcon}
-                  alt=""
-                />
-                <span
-                  className={cn({
-                    'text-destructive': rider.isHighlight,
-                  })}
-                >
-                  {getVillageTitle(rider.mainStation)}
-                </span>
+                <div className="flex items-center gap-1 text-xs">
+                  <MapPin
+                    className={cn('size-3', {
+                      'text-destructive': rider.isLive,
+                    })}
+                  />
+                  <span
+                    className={cn({
+                      'text-destructive': rider.isLive,
+                    })}
+                  >
+                    {rider.distance.toFixed(2)} km
+                  </span>
+                  {rider.isLive && (
+                    <Button
+                      variant="destructive"
+                      size="sm"
+                      className="text-[11px] h-4 px-1.5 rounded-sm"
+                    >
+                      Live
+                    </Button>
+                  )}
+                </div>
               </div>
-            )}
 
-            <div className="flex items-center gap-1 text-sm">
-              <MapPin
-                className={cn('size-3', { 'text-destructive': rider.isLive })}
-              />
-              <span
-                className={cn({
-                  'text-destructive': rider.isLive,
-                })}
-              >
-                {rider.distance.toFixed(2)} km
-              </span>
-              {rider.isLive && (
-                <Button
-                  variant="destructive"
-                  size="sm"
-                  className="text-[11px] h-4 px-1.5 rounded-sm"
-                >
-                  Live
-                </Button>
-              )}
+              <div className="w-[30px]">
+                <img src={vehicleIcon} alt="" className="w-full" />
+              </div>
             </div>
 
-            <div
-              className="flex gap-2 mt-2"
-              onClick={(e) => e.stopPropagation()}
-            >
-              <Link
-                to={
-                  rider.contactNo2 ? `https://wa.me/+88${rider.contactNo2}` : ''
-                }
+            <div className="flex justify-between items-center gap-4">
+              <div
+                className="flex gap-1 mt-1"
                 onClick={(e) => e.stopPropagation()}
               >
-                <Button size="sm" disabled={!rider.contactNo2}>
-                  <BsWhatsapp />
-                  WhatsApp
-                </Button>
-              </Link>
+                <Link
+                  to={`https://wa.me/+88${rider.contactNo2 || rider.contactNo1}`}
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <Button
+                    size="sm"
+                    className="text-[8px] h-6 gap-1 px-2 rounded-md"
+                  >
+                    <PiWhatsappLogoFill />
+                    WhatsApp
+                  </Button>
+                </Link>
 
-              <Link
-                to={`tel:${rider.contactNo1}`}
-                onClick={(e) => e.stopPropagation()}
-              >
-                <Button size="sm">
-                  <Phone />
-                  Call
-                </Button>
-              </Link>
+                <Link
+                  to={`tel:${rider.contactNo1}`}
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <Button
+                    size="sm"
+                    className="text-[8px] h-6 gap-1 px-2 rounded-md"
+                  >
+                    <PiPhoneFill />
+                    Call
+                  </Button>
+                </Link>
+              </div>
+
+              <div className="flex items-center gap-1">
+                <p className="text-[10px] text-muted-foreground">10</p>
+                <button
+                  className={cn(
+                    'text-[10px] h-5 w-14 px-2 rounded-sm flex items-center gap-1',
+                    {
+                      'bg-tone-600': isSaved,
+                      'border-tone-600 border': !isSaved,
+                    }
+                  )}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setIsSaved(!isSaved);
+                  }}
+                >
+                  <span>
+                    {isSaved ? (
+                      <PiStarFill className="text-yellow-300" size={10} />
+                    ) : (
+                      <PiStar className="" size={10} />
+                    )}
+                  </span>
+                  <span>{isSaved ? 'Saved' : 'Save'}</span>
+                </button>
+              </div>
             </div>
           </div>
         </div>
