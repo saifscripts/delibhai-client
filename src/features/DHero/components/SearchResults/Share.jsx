3abfc60b7beda4f26da7ea443c21b9849fbdc06c
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-12-29 04:22:04 +0600

    [vehicle/search]: update share modal on rider card

--- src/features/DHero/components/SearchResults/Share.jsx
+++ src/features/DHero/components/SearchResults/Share.jsx
@@ -1,11 +1,14 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover';
-import { Link } from 'lucide-react';
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Link, Shield } from 'lucide-react';
 import * as React from 'react';
 import { PiShareFat } from 'react-icons/pi';
 import { TbBrandWhatsapp } from 'react-icons/tb';
@@ -15,40 +18,60 @@ export function Share({ rider }) {
   const [open, setOpen] = React.useState(false);
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
         <button className="flex items-center gap-1">
           <span>
             <PiShareFat className="" size={16} />
           </span>
           <span>Share</span>
         </button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0 mr-4">
-        <div className="flex flex-col py-1">
-          <button
+      </DialogTrigger>
+      <DialogContent className="p-16 text-center m-8 rounded-2xl">
+        <div className="flex items-center justify-center">
+          <div className="relative size-16">
+            <Shield
+              size={56}
+              className="text-primary absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            />
+            <PiShareFat
+              size={24}
+              className="text-primary absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            />
+          </div>
+        </div>
+        <DialogTitle className="text-xl text-primary">
+          প্রিয়জনদের সাথে শেয়ার করুন রাইডারের প্রোফাইল
+        </DialogTitle>
+        <DialogDescription>
+          এটি যাত্রার স্বচ্ছতা নিশ্চিত করে এবং জরুরি প্রয়োজনে সহজে যোগাযোগে
+          সাহায্য করবে
+        </DialogDescription>
+        <div className="flex flex-col py-1 gap-2">
+          <Button asChild>
+            <a
+              href={`whatsapp://send?text=${window.location.origin}/profile/${rider._id}`}
+              data-action="share/whatsapp/share"
+            >
+              <TbBrandWhatsapp />
+              Share on WhatsApp
+            </a>
+          </Button>
+
+          <Button
+            variant="outline"
             onClick={() => {
               navigator.clipboard.writeText(
                 `${window.location.origin}/profile/${rider._id}`
               );
               toast.success('Link copied to clipboard');
             }}
-            className="flex items-center gap-2 hover:bg-gray-100 px-2 py-1"
           >
             <Link size={16} />
             Copy Link
-          </button>
-
-          <a
-            href={`whatsapp://send?text=${window.location.origin}/profile/${rider._id}`}
-            data-action="share/whatsapp/share"
-            className="flex items-center gap-2 hover:bg-gray-100 px-2 py-1"
-          >
-            <TbBrandWhatsapp />
-            Share on WhatsApp
-          </a>
+          </Button>
         </div>
-      </PopoverContent>
-    </Popover>
+      </DialogContent>
+    </Dialog>
   );
 }
