411e68466411c8bdbf91b32b3dd8996d1ede7638
Md Saif Ullah <mdsaifullah1302@gmail.com>
2025-01-07 15:12:04 +0600

    setup socket.io

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,24 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { Toaster } from "sonner";
-import { ThemeProvider } from "./components/theme-provider.js";
-import { AuthProvider } from "./contexts/auth.context.jsx";
-import { GeolocationProvider } from "./contexts/location.context.jsx";
-import "./index.css";
-import router from "./router.jsx";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { io } from 'socket.io-client';
+import { Toaster } from 'sonner';
+import { ThemeProvider } from './components/theme-provider.js';
+import { AuthProvider } from './contexts/auth.context.jsx';
+import { GeolocationProvider } from './contexts/location.context.jsx';
+import './index.css';
+import router from './router.jsx';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const socket = io('http://localhost:5000');
+
+socket.on('connection', (value) => {
+  console.log(value);
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
@@ -23,5 +30,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         </GeolocationProvider>
       </AuthProvider>
     </QueryClientProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
